9e0ab755fb61f087e0ed88cdb4dd18f30e7bb408
moryno <mauricenganga41@gmail.com>
2024-08-03 15:00:54 +0300

    Update the user crontroller return object for login

--- backend/API/Controllers/UserController.cs
+++ backend/API/Controllers/UserController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.Users;
+using Domain;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LoginController : ControllerBase
+
+    public class UserController : BaseController
     {
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(Login.Query query)
+        {
+            return await Mediator.Send(query);
+        }
     }
 }
