ebea2aa7b2f77e9e77de5bf48ec24307b974db17
moryno <mauricenganga41@gmail.com>
2024-08-03 15:02:16 +0300

    Add User class that can return user object return after login

---
+++ backend/Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class User
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
