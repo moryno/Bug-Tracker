943e15b866d1e939f8ff87d6a28eddff7b12a1f6
moryno <mauricenganga41@gmail.com>
2024-08-03 09:03:08 +0300

    Add rest exception to the edit class

--- backend/Application/Projects/Edit.cs
+++ backend/Application/Projects/Edit.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Errors;
 using Domain;
 using FluentValidation;
 using MediatR;
 using Persistence;
+using System.Net;
 
 namespace Application.Projects
 {
@@ -52,7 +50,7 @@ namespace Application.Projects
             {
                 Project project = await _context.Projects.FindAsync(request.Id);
                 if (project == null)
-                    throw new Exception("Could not find project.");
+                    throw new RestException(HttpStatusCode.NotFound, new { project = "Not found." });
 
                 project.ProjectName = request.ProjectName ?? project.ProjectName;
                 project.ProjectGroup = request.ProjectGroup ?? project.ProjectGroup;
