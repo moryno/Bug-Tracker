9f1dc5d6ac508cabc3d6e3c16bf18a94a41a16ea
moryno <mauricenganga41@gmail.com>
2024-08-10 11:20:59 +0300

    Add class to get currently logged in user

---
+++ backend/Application/Users/CurrentUser.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users
+{
+    public class CurrentUser
+    {
+        public class Query : IRequest<User> { }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly SignInManager<AppUser> _signInManager;
+            private readonly IUserAccessor _userAccessor;
+            private readonly IJwtGenerator _jwtGenerator;
+
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IUserAccessor userAccessor, IJwtGenerator jwtGenerator)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+                _userAccessor = userAccessor;
+                _jwtGenerator = jwtGenerator;
+            }
+
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                AppUser user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUserName());
+
+                return new User
+                {
+                    Email = user.Email,
+                    FullName = user.DisplayName,
+                    UserName = user.UserName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    Image = null
+                };
+            }
+        }
+    }
+}
