5041a9f7b65e8c4c069a30624121e417d049312f
moryno <mauricenganga41@gmail.com>
2024-08-10 13:25:41 +0300

    Add reference to join table too

--- backend/Domain/Project.cs
+++ backend/Domain/Project.cs
@@ -6,6 +6,10 @@ public class Project
 
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedUser { get; set; } = string.Empty;
+    public DateTime UpdatedDate { get; set; }
+    public string UpdatedUser { get; set; } = string.Empty;
 
     public string Priority { get; set; } = string.Empty;
 
@@ -14,4 +18,5 @@ public class Project
     public string Description { get; set; } = string.Empty;
     public string ProjectGroup { get; set; } = string.Empty;
     public bool Private { get; set; }
+    public ICollection<UserProject> UserProjects { get; set; }
 }
