26cba3accab58cd9261e33bf935a8b4b5f3512a7
moryno <mauricenganga41@gmail.com>
2024-08-10 13:24:32 +0300

    Add join table userprojects to define relationship between user and project

---
+++ backend/Domain/UserProject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class UserProject
+    {
+        public string AppUserId { get; set; } = string.Empty;
+        public AppUser AppUser { get; set; } 
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; } 
+        public DateTime DateAssigned { get; set; }
+        public bool IsOwner { get; set; }
+    }
+}
