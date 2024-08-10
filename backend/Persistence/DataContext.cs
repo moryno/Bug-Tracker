40555d91f69a4cb4e7907715e84292853736b5a1
moryno <mauricenganga41@gmail.com>
2024-08-10 13:26:32 +0300

    Add dbset for userprojects and define keys and relationships for the table

--- backend/Persistence/DataContext.cs
+++ backend/Persistence/DataContext.cs
@@ -12,11 +12,25 @@ public class DataContext : IdentityDbContext<AppUser>
 
     public DbSet<Project> Projects { get; set; }
     public DbSet<Bug> Bugs { get; set; }
-    //public DbSet<ProjectComment> ProjectComments { get; set; }
+    public DbSet<UserProject> UserProjects { get; set; }
     //public DbSet<BugComment> BugComments { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<UserProject>(x => x.HasKey(ua => new
+        {
+            ua.AppUserId,
+            ua.ProjectId
+        }));
+        builder.Entity<UserProject>()
+            .HasOne(a => a.AppUser)
+            .WithMany(u => u.UserProjects)
+            .HasForeignKey(a => a.AppUserId);
+        builder.Entity<UserProject>()
+            .HasOne(p => p.Project)
+            .WithMany(u => u.UserProjects)
+            .HasForeignKey(p => p.ProjectId);
     }
 }
