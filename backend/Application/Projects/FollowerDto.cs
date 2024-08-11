bbb245e0231138f77a470690fe3dd42329504a0d
moryno <mauricenganga41@gmail.com>
2024-08-11 14:34:17 +0300

    Add Follower class

---
+++ backend/Application/Projects/FollowerDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Projects
+{
+    public class FollowerDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public bool IsOwner { get; set; }
+    }
+}
