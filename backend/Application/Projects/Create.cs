0feb19bf279650a631119732684ef6d1a55087f2
moryno <mauricenganga41@gmail.com>
2024-08-11 09:32:12 +0300

    Edit the model from request

--- backend/Application/Projects/Create.cs
+++ backend/Application/Projects/Create.cs
@@ -1,6 +1,8 @@
-﻿using Domain;
+﻿using Application.Interfaces;
+using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System;
 using System.Collections.Generic;
@@ -19,14 +21,18 @@ namespace Application.Projects
 
             public DateTime StartDate { get; set; }
             public DateTime EndDate { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public string CreatedUser { get; set; } = string.Empty;
+            public DateTime UpdatedDate { get; set; }
+            public string UpdatedUser { get; set; } = string.Empty;
 
             public string Priority { get; set; } = string.Empty;
 
-            public string Owner { get; set; } = string.Empty;
+            public string? Owner { get; set; } = string.Empty;
 
             public string Description { get; set; } = string.Empty;
             public string ProjectGroup { get; set; } = string.Empty;
-            public bool Private { get; set; }
+            public bool Private { get; set; } = false;
         }
 
         public class CommandValidator : AbstractValidator<Command>
@@ -36,20 +42,23 @@ namespace Application.Projects
                 RuleFor(x => x.ProjectName).NotEmpty().MinimumLength(3);
                 RuleFor(x => x.StartDate).NotEmpty();
                 RuleFor(x => x.Priority).NotEmpty();
-                RuleFor(x => x.Private).NotEmpty();
             }
         }
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _context = context;
+                _userAccessor = userAccessor;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user =  await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUserName());
+
                 Project project = new()
                 {
                     Id = request.Id,
@@ -57,13 +66,28 @@ namespace Application.Projects
                     StartDate = request.StartDate,
                     EndDate = request.EndDate,
                     Priority = request.Priority,
-                    Owner = request.Owner,
+                    Owner = request.Owner ?? _userAccessor.GetCurrentUserName(),
                     Description = request.Description,
                     ProjectGroup = request.ProjectGroup,
                     Private = request.Private,
+                    CreatedDate = DateTime.Now,
+                    CreatedUser = user.UserName,
+                    UpdatedDate = DateTime.Now,
+                    UpdatedUser = user.UserName,
                 };
 
                 _context.Projects.Add(project);
+
+                UserProject userProjects = new()
+                {
+                    AppUser = user,
+                    Project = project,
+                    IsOwner = true,
+                    DateAssigned = DateTime.Now,
+                };
+
+                _context.UserProjects.Add(userProjects);
+
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Unit.Value;
