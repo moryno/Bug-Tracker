82065be20823366cb1133507b43b700fa7273f44
moryno <mauricenganga41@gmail.com>
2024-08-11 11:14:28 +0300

    Add dataset and relationship entity for project and bug comments

--- backend/Persistence/DataContext.cs
+++ backend/Persistence/DataContext.cs
@@ -13,7 +13,8 @@ public class DataContext : IdentityDbContext<AppUser>
     public DbSet<Project> Projects { get; set; }
     public DbSet<Bug> Bugs { get; set; }
     public DbSet<UserProject> UserProjects { get; set; }
-    //public DbSet<BugComment> BugComments { get; set; }
+    public DbSet<BugComment> BugComments { get; set; }
+    public DbSet<ProjectComment> ProjectComments { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -31,6 +32,34 @@ public class DataContext : IdentityDbContext<AppUser>
         builder.Entity<UserProject>()
             .HasOne(p => p.Project)
             .WithMany(u => u.UserProjects)
+            .HasForeignKey(p => p.ProjectId);
+
+        builder.Entity<BugComment>(x => x.HasKey(bc => new
+        {
+            bc.AppUserId,
+            bc.BugId
+        }));
+        builder.Entity<BugComment>()
+            .HasOne(b => b.Bug)
+            .WithMany(bc => bc.BugComments)
+            .HasForeignKey(b => b.BugId);
+        builder.Entity<BugComment>()
+            .HasOne(a => a.AppUser)
+            .WithMany(bc => bc.BugComments)
+            .HasForeignKey(a => a.AppUserId);
+
+        builder.Entity<ProjectComment>(x => x.HasKey(pc => new
+        {
+            pc.AppUserId,
+            pc.ProjectId
+        }));
+        builder.Entity<ProjectComment>()    
+            .HasOne(a => a.AppUser)
+            .WithMany(pc => pc.ProjectComments)
+            .HasForeignKey(a => a.AppUserId);
+        builder.Entity<ProjectComment>()
+            .HasOne(p => p.Project)
+            .WithMany(pc => pc.ProjectComments)
             .HasForeignKey(p => p.ProjectId);
     }
 }
