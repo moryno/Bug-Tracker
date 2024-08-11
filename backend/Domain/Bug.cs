58dd1232790126b1b422372e1cc468030cc547dd
moryno <mauricenganga41@gmail.com>
2024-08-11 11:10:54 +0300

    Add relationship with bu comments

--- backend/Domain/Bug.cs
+++ backend/Domain/Bug.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain
+﻿namespace Domain
 {
     public class Bug
     {
         public Guid Id { get; set; }
-        public string ProjectName { get; set; } = string.Empty;
+        public Guid Project { get; set; }
         public string Description { get; set; } = string.Empty;
 
         public string BugName { get; set; } = string.Empty;
-        public string Assignee { get; set; }
+        public string Assignee { get; set; } = string.Empty;
         public string Severity { get; set; } = string.Empty;
         public string Classification { get; set; } = string.Empty;
         public DateTime DueDate { get; set; }
         public string BugStatus { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string CreatedUser { get; set; } = string.Empty;
+        public DateTime UpdatedDate { get; set; }
+        public string UpdatedUser { get; set; } = string.Empty;
+        public ICollection<BugComment> BugComments { get; set; } = new List<BugComment>();
     }
 }
