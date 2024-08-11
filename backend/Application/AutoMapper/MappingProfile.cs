87c6b1efb08f4ffaf74ada8ff8fcc8630660cf45
moryno <mauricenganga41@gmail.com>
2024-08-11 14:32:49 +0300

    Add auto mapper profile

---
+++ backend/Application/AutoMapper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using Application.Projects;
+using AutoMapper;
+using Domain;
+
+namespace Application.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Project, ProjectDto>();
+            CreateMap<UserProject, FollowerDto>()
+                .ForMember(d => d.UserName, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.FullName, o => o.MapFrom(s => s.AppUser.DisplayName));
+        }
+    }
+}
