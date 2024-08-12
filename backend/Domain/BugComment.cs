be03b8beee33bf34ba6e6f3eb530ab5352a59d63
moryno <mauricenganga41@gmail.com>
2024-08-12 05:43:26 +0300

    Add virtual property in refrencing relationship

--- backend/Domain/BugComment.cs
+++ backend/Domain/BugComment.cs
@@ -3,9 +3,9 @@
     public class BugComment
     {
         public string AppUserId { get; set; } = string.Empty;
-        public AppUser AppUser { get; set; } = new AppUser();
+        public virtual AppUser AppUser { get; set; } = new AppUser();
         public Guid BugId { get; set; } = Guid.Empty;
-        public Bug Bug { get; set; } = new Bug();
+        public virtual Bug Bug { get; set; } = new Bug();
         public string Description { get; set; } = string.Empty;
         public DateTime DateCommented { get; set; }
     }
