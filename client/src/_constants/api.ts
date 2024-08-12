c9f345a183de5b48c5fb8aa811ae4a936917b204
moryno <mauricenganga41@gmail.com>
2024-08-12 12:13:21 +0300

    Add apis for auth and project

---
+++ client/src/_constants/api.ts
@@ -0,0 +1,17 @@
+// AUTH
+export const LOGIN_API = "/user/login";
+export const REGISTER_API = "/user/register";
+export const LOGOUT_API = "/logout/logout";
+
+// USERS
+export const USERS_API = "/users";
+
+// PROJECTS
+export const PROJECTS_API = "/projects";
+
+// BUGS
+export const BUGS_API = "/bugs";
+
+// COMMENTS
+export const PROJECT_COMMENTS_API = "/projects/comments";
+export const BUG_COMMENTS_API = "/bugs/comments";
