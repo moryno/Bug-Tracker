bd84193af82bf8d807cbbdf042a505e878456a13
moryno <mauricenganga41@gmail.com>
2024-08-12 05:44:00 +0300

    Add virtual property in refrencing relationship

--- backend/Domain/ProjectComment.cs
+++ backend/Domain/ProjectComment.cs
@@ -3,9 +3,9 @@
     public class ProjectComment
     {
         public string AppUserId { get; set; } = string.Empty;
-        public AppUser AppUser { get; set; }
+        public virtual AppUser AppUser { get; set; }
         public Guid ProjectId { get; set; }
-        public Project Project { get; set; }
+        public virtual Project Project { get; set; }
         public string Description { get; set; } = string.Empty;
         public DateTime DateCommented { get; set; }
     }
