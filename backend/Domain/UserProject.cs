b56b2588e731024955b855bf040a1247c3f5960f
moryno <mauricenganga41@gmail.com>
2024-08-12 05:44:23 +0300

    Add virtual property in refrencing relationship

--- backend/Domain/UserProject.cs
+++ backend/Domain/UserProject.cs
@@ -3,9 +3,9 @@
     public class UserProject
     {
         public string AppUserId { get; set; } = string.Empty;
-        public AppUser AppUser { get; set; } 
+        public virtual AppUser AppUser { get; set; } 
         public Guid ProjectId { get; set; }
-        public Project Project { get; set; } 
+        public virtual Project Project { get; set; } 
         public DateTime DateAssigned { get; set; }
         public bool IsOwner { get; set; }
     }
