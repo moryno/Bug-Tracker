1b9ed3bf58517dc619708ec65f261d1ebf0c2306
moryno <mauricenganga41@gmail.com>
2024-08-12 05:41:06 +0300

    Add class to filter request from owner

---
+++ backend/Infrastructure/Security/IsOwnerRequirement.cs
@@ -0,0 +1,49 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Persistence;
+using System.Security.Claims;
+
+namespace Infrastructure.Security
+{
+    public class IsOwnerRequirement : IAuthorizationRequirement
+    {
+    }
+
+    public class IsOwnerRequirementHandler : AuthorizationHandler<IsOwnerRequirement>
+    {
+        private readonly DataContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public IsOwnerRequirementHandler(DataContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsOwnerRequirement requirement)
+        {
+            if (context.Resource is AuthorizationFilterContext authContext) {
+
+                var currentUserName = _httpContextAccessor.HttpContext.User?.Claims?.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+                var projectId = Guid.Parse(authContext.RouteData.Values["id"].ToString());
+
+                var project = _context.Projects.FindAsync(projectId).Result;
+
+                var owner = project?.UserProjects.FirstOrDefault(x => x.IsOwner);
+
+                if (owner?.AppUser?.UserName == currentUserName)
+                    context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
