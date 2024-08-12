44716ed080b9c17cf0cdc10772cd68c2572f51e7
moryno <mauricenganga41@gmail.com>
2024-08-12 12:19:07 +0300

    Done

--- client/src/_helpers/commonHelper.ts
+++ client/src/_helpers/commonHelper.ts
@@ -10,3 +10,16 @@ export const hardRefreshAndEmptyCache = async () => {
   }
   window.location.reload();
 };
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat("en-US", {
+    dateStyle: "medium",
+    timeStyle: "short",
+  }).format(new Date(date));
+}
+export const getTitleCaseSentence = (str: string) => {
+  if (!str) {
+    return "";
+  }
+  return str.toLowerCase().replace(/\b\w/g, (s) => s.toUpperCase());
+};
