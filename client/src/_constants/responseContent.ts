ded394a5f756e059209f62c1a3dce8d22d4f2402
moryno <mauricenganga41@gmail.com>
2024-08-12 12:13:54 +0300

    Add helper for response messages

---
+++ client/src/_constants/responseContent.ts
@@ -0,0 +1,35 @@
+import {
+  BUG_COMMENTS_API,
+  BUGS_API,
+  PROJECT_COMMENTS_API,
+  PROJECTS_API,
+  REGISTER_API,
+} from "./api";
+interface SuccessResponseContent {
+  [key: string]: {
+    message: string;
+    description: string;
+  };
+}
+export const successResponseContent: SuccessResponseContent = {
+  [REGISTER_API]: {
+    message: "Account",
+    description: "Register was successful",
+  },
+  [PROJECTS_API]: {
+    message: "Success",
+    description: "Creating project was successful",
+  },
+  [BUGS_API]: {
+    message: "Success",
+    description: "Creating bugs was successful",
+  },
+  [PROJECT_COMMENTS_API]: {
+    message: "Success",
+    description: "Saving comment was successful",
+  },
+  [BUG_COMMENTS_API]: {
+    message: "Success",
+    description: "Saving comment was successful",
+  },
+};
