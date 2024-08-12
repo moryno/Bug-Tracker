3fe45d7ebd56f1b0eef2d72c86e3c33a4d73034a
moryno <mauricenganga41@gmail.com>
2024-08-12 05:42:22 +0300

    Add virtual property in refrencing relationship

--- backend/Domain/AppUser.cs
+++ backend/Domain/AppUser.cs
@@ -5,8 +5,8 @@ namespace Domain
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; } = string.Empty;
-        public ICollection<UserProject> UserProjects { get; set; } = new List<UserProject>();
-        public ICollection<ProjectComment> ProjectComments { get; set; } = new List<ProjectComment>();
-        public ICollection<BugComment> BugComments { get; set; } = new List<BugComment>();
+        public virtual ICollection<UserProject> UserProjects { get; set; } = new List<UserProject>();
+        public virtual ICollection<ProjectComment> ProjectComments { get; set; } = new List<ProjectComment>();
+        public virtual ICollection<BugComment> BugComments { get; set; } = new List<BugComment>();
     }
 }
