37ed122a1df5ca80c68ae36051faac565d1a2536
moryno <mauricenganga41@gmail.com>
2024-08-13 10:24:58 +0300

    Add posting functionality

--- client/src/pages/projects/components/ProjectForm.tsx
+++ client/src/pages/projects/components/ProjectForm.tsx
@@ -1,95 +1,161 @@
-import React from "react";
-import { Col, DatePicker, Form, Input, Row, Select } from "antd";
+import { DomianEnum } from "_constants";
+import { isMomentObject } from "_helpers";
+import { useCreateService } from "_hooks";
+import { ContainerDrawer } from "_lib";
+import { ProjectService } from "_services";
+import { Col, DatePicker, Form, Input, message, Row, Select } from "antd";
+import { drawerType, IProject } from "interfaces";
+import moment from "moment";
+import { ChangeEvent, useCallback, useState } from "react";
 const { Option } = Select;
 
-const ProjectForm = () => {
+const initialData: IProject = {
+  projectName: "",
+  priority: "",
+  description: ""
+}
+
+const ProjectForm:React.FC<drawerType> = ({ onClose, open }) => {
+  const [formData, setFormData] = useState<IProject>(initialData);
+  const [loading, setLoading] = useState(false);
+  const createProject = useCreateService(ProjectService.createProject, DomianEnum.PROJECTS)
+
+  const handleValueChange = useCallback((e: ChangeEvent<HTMLInputElement | HTMLSelectElement>): void => {
+    setFormData(prev => (
+      { ...prev, ...e }
+    ));
+  },[]);
+
+  const onFinish = useCallback(async () => {
+    if(!formData.description)
+      return message.warning("Project description is required.");
+
+   setLoading(true);
+    try {
+    
+      const params = {
+        ...formData,
+        owner: formData.owner ? formData.owner : null,
+        priority: formData.priority ? formData.priority : null,
+        startDate: formData?.startDate ? isMomentObject(formData?.startDate)
+         ? formData?.startDate?.toISOString()
+         : moment(formData?.startDate)?.toISOString()
+         : null,
+         endDate: formData?.endDate ? isMomentObject(formData?.endDate)
+         ? formData?.endDate?.toISOString()
+         : moment(formData?.endDate)?.toISOString()
+         : null
+      }
+      await createProject.mutateAsync(params);
+      setLoading(false);
+      onClose()
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [createProject, formData, onClose]);
+
   return (
-    <Form layout="vertical">
-      <Row gutter={16}>
-        <Col span={24}>
-          <Form.Item
-            name="name"
-            label="Project Name"
-            rules={[{ required: true, message: "Please enter project name" }]}
-          >
-            <Input placeholder="Please enter project name" />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Form.Item
-            name="startDate"
-            label="Start Date"
-            rules={[
-              { required: true, message: "Please choose the start date" },
-            ]}
-          >
-            <DatePicker
-              style={{ width: "100%" }}
-              getPopupContainer={(trigger) => trigger.parentElement!}
-            />
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            name="endDate"
-            label="End Date"
-            rules={[{ required: true, message: "Please choose the end date" }]}
-          >
-            <DatePicker
-              style={{ width: "100%" }}
-              getPopupContainer={(trigger) => trigger.parentElement!}
-            />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Form.Item
-            name="owner"
-            label="Owner"
-            rules={[{ required: true, message: "Please select an owner" }]}
-          >
-            <Select placeholder="Please select an owner">
-              <Option value="xiao">Xiaoxiao Fu</Option>
-              <Option value="mao">Maomao Zhou</Option>
-            </Select>
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            name="priority"
-            label="Priority"
-            rules={[{ required: true, message: "Please select an priority" }]}
-          >
-            <Select placeholder="Please select an owner">
-              <Option value="xiao">Xiaoxiao Fu</Option>
-              <Option value="mao">Maomao Zhou</Option>
-            </Select>
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={24}>
-          <Form.Item
-            name="description"
-            label="Description"
-            rules={[
-              {
-                required: true,
-                message: "please enter url description",
-              },
-            ]}
-          >
-            <Input.TextArea
-              rows={4}
-              placeholder="please enter url description"
-            />
-          </Form.Item>
-        </Col>
-      </Row>
-    </Form>
+    <ContainerDrawer 
+       onFinish={onFinish} 
+       onClose={onClose} 
+       open={open} 
+       loading={loading}
+       title={"New Project"} 
+       width={720}
+     >
+      <Form 
+        onFinish={onFinish}
+        initialValues={formData}
+        onValuesChange={handleValueChange}
+        layout="vertical"
+      >
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              name="projectName"
+              label="Project Name"
+              rules={[{ required: true, message: "Please enter project name" }]}
+            >
+              <Input placeholder="Please enter project name" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="startDate"
+              label="Start Date"
+              style={{ width: '100%' }}
+              rules={[
+                { required: true, message: "Please choose the start date" },
+              ]}
+            >
+              <DatePicker
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="endDate"
+              label="End Date"
+              rules={[{ required: false, message: "Please choose the end date" }]}
+            >
+              <DatePicker
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="owner"
+              label="Owner"
+              rules={[{ required: false, message: "Please select an owner" }]}
+            >
+              <Select placeholder="Please select an owner" allowClear>
+                <Option value="xiao">Xiaoxiao Fu</Option>
+                <Option value="mao">Maomao Zhou</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="priority"
+              label="Priority"
+              rules={[{ required: false, message: "Please select an priority" }]}
+            >
+              <Select placeholder="Please select priority" allowClear>
+                <Option value="High">High</Option>
+                <Option value="Medium">Medium</Option>
+                <Option value="Low">Low</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              name="description"
+              label="Description"
+              rules={[
+                {
+                  required: true,
+                  message: "please enter description",
+                },
+              ]}
+            >
+              <Input.TextArea
+                rows={4}
+                placeholder="please enter description"
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </ContainerDrawer>
   );
 };
 
