9b52f2662457eff7ca16f6510e87d3e81149c436
moryno <mauricenganga41@gmail.com>
2024-08-13 10:25:41 +0300

    Modify the model on creation

--- backend/Application/Projects/Create.cs
+++ backend/Application/Projects/Create.cs
@@ -14,13 +14,13 @@ namespace Application.Projects
             public string ProjectName { get; set; } = string.Empty;
 
             public DateTime StartDate { get; set; }
-            public DateTime EndDate { get; set; }
+            public DateTime? EndDate { get; set; }
             public DateTime CreatedDate { get; set; }
             public string CreatedUser { get; set; } = string.Empty;
             public DateTime UpdatedDate { get; set; }
             public string UpdatedUser { get; set; } = string.Empty;
 
-            public string Priority { get; set; } = string.Empty;
+            public string? Priority { get; set; } = string.Empty;
 
             public string? Owner { get; set; } = string.Empty;
 
@@ -35,7 +35,7 @@ namespace Application.Projects
             {
                 RuleFor(x => x.ProjectName).NotEmpty().MinimumLength(3);
                 RuleFor(x => x.StartDate).NotEmpty();
-                RuleFor(x => x.Priority).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
             }
         }
         public class Handler : IRequestHandler<Command>
@@ -57,16 +57,16 @@ namespace Application.Projects
                 {
                     ProjectName = request.ProjectName,
                     StartDate = request.StartDate,
-                    EndDate = request.EndDate,
-                    Priority = request.Priority,
+                    EndDate = request.EndDate ?? request.StartDate.AddDays(90),
+                    Priority = request.Priority ?? "High",
                     Owner = request.Owner ?? user.UserName,
                     Description = request.Description,
                     ProjectGroup = request.ProjectGroup,
                     Private = request.Private,
                     CreatedDate = DateTime.Now,
-                    CreatedUser = user.UserName,
+                    CreatedUser = user.DisplayName,
                     UpdatedDate = DateTime.Now,
-                    UpdatedUser = user.UserName,
+                    UpdatedUser = user.DisplayName,
                 };
 
                 _context.Projects.Add(project);
