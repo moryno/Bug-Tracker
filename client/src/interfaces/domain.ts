f221ae15a7986e86ad782f52f9c522511ae93134
moryno <mauricenganga41@gmail.com>
2024-08-13 05:23:03 +0300

    Add domain interface

---
+++ client/src/interfaces/domain.ts
@@ -0,0 +1,62 @@
+export interface IProject {
+  id?: string;
+  projectName: string;
+  startDate: Date;
+  endDate: Date;
+  createdDate?: Date;
+  createdUser?: string;
+  updatedDate?: Date;
+  updatedUser?: string;
+  priority: string;
+  owner?: string;
+  description: string;
+  projectGroup?: string;
+  private?: boolean;
+  assignee?: IFollowerDto[];
+}
+
+export interface IFollowerDto {
+  userName: string;
+  fullName: string;
+  image: string;
+  isOwner: boolean;
+}
+export interface IBug {
+  id: string;
+  project: string;
+  description: string;
+  bugName: string;
+  assignee: string;
+  severity: string;
+  classification: string;
+  dueDate: Date;
+  bugStatus: string;
+  createdDate: Date;
+  createdUser: string;
+  updatedDate: Date;
+  updatedUser: string;
+  bugComments: IBugComment[];
+}
+export interface IBugComment {
+  appUserId: string;
+  appUser: IUser;
+  bugId: string;
+  bug: IBug;
+  description: string;
+  dateCommented: Date;
+}
+export interface IProjectComment {
+  appUserId: string;
+  appUser: IUser;
+  projectId: string;
+  project: IProject;
+  description: string;
+  dateCommented: Date;
+}
+export interface IUser {
+  fullName: string;
+  email: string;
+  image: string;
+  token: string;
+  userName: string;
+}
