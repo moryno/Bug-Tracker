9296162a445eb0657670ed519d42787da4dfcb4f
moryno <mauricenganga41@gmail.com>
2024-08-16 08:21:30 +0300

    Update user information from response from backend

--- client/src/pages/projects/components/ProjectDetail.tsx
+++ client/src/pages/projects/components/ProjectDetail.tsx
@@ -1,8 +1,30 @@
-import React from "react";
 import { Card, Tabs, TabsProps } from "antd";
 import ProjectInformation from "./ProjectInformation";
 import { Comment, CommentForm, DetailPage, DescriptionComponent } from "_lib";
 import ProjectDetailHeader from "./ProjectDetailHeader";
+import { useParams } from "react-router-dom";
+import { useGetById } from "_hooks";
+import { ProjectService } from "_services";
+import { DomianEnum } from "_constants";
+
+
+
+const ProjectDetail = () => {
+ const { id } = useParams();
+ const { isLoading, data } = useGetById(ProjectService.getProject, DomianEnum.PROJECTS, id);
+
+  const onChange = (key: string) => {
+    console.log(key);
+  };
+  return (
+    <DetailPage>
+      <ProjectDetailHeader title={data?.data?.projectName} />
+      <DescriptionComponent description={data?.data?.description} />
+      <ProjectInformation project={data?.data} />
+      <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+    </DetailPage>
+  );
+};
 
 const items: TabsProps["items"] = [
   {
@@ -26,19 +48,4 @@ const items: TabsProps["items"] = [
     children: "Content of Tab Pane 3",
   },
 ];
-
-const ProjectDetail = () => {
-  const onChange = (key: string) => {
-    console.log(key);
-  };
-  return (
-    <DetailPage>
-      <ProjectDetailHeader />
-      <DescriptionComponent description="A dog is a type of domesticated animal. Known for its loyalty and faithfulness,the world." />
-      <ProjectInformation />
-      <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
-    </DetailPage>
-  );
-};
-
 export default ProjectDetail;
