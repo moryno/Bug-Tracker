2d068814ffab1d8b83e481dfe6967037401c62c7
moryno <mauricenganga41@gmail.com>
2024-08-16 08:19:12 +0300

    Add tanstack query devtool

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import { Provider } from "react-redux";
 import { persistor, store } from "_redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 
 function App() {
   const queryClient = new QueryClient();
@@ -20,6 +22,7 @@ function App() {
             <AppLayout />
           </AuthRoutes>
         </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
      </AppThemeProvider>
     </PersistGate>
