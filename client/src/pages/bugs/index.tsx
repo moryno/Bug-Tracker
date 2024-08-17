9443301aa432149b003301c9b8d68efd44cab600
moryno <mauricenganga41@gmail.com>
2024-08-17 13:42:23 +0300

    Modify the props

--- client/src/pages/bugs/index.tsx
+++ client/src/pages/bugs/index.tsx
@@ -1,10 +1,19 @@
 import { GroupPage } from '_lib'
 import BugForm from './components/BugForm'
 import { DomianEnum } from '_constants'
+import { BugService } from '_services'
+import { bugColumns } from './columns'
 
 const Bugs = () => {
   return (
-    <GroupPage title={DomianEnum.BUGS} FormComponent={BugForm} columns={[]} />
+    <GroupPage 
+    title={DomianEnum.BUGS} 
+    FormComponent={BugForm} 
+    columns={bugColumns}
+    getAllService={BugService.getBugs}
+    getDetailService={BugService.getBug}
+    deleteService={BugService.deleteBug}
+     />
   )
 }
 
