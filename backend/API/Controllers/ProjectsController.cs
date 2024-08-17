946ef4e1c106612cc5c5afc58f3ec6ca9618586b
moryno <mauricenganga41@gmail.com>
2024-08-17 11:31:24 +0300

    Modify the create service to only create project with unique names

--- backend/API/Controllers/ProjectsController.cs
+++ backend/API/Controllers/ProjectsController.cs
@@ -12,14 +12,14 @@ namespace API.Controllers
 
         [HttpGet]
         [Authorize]
-        public async Task<ActionResult<List<Project>>> Get ()
+        public async Task<ActionResult<List<ProjectDto>>> Get ()
         {
             return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         [Authorize]
-        public async Task<ActionResult<Project>> GetById (Guid id)
+        public async Task<ActionResult<ProjectDto>> GetById (Guid id)
         {
             return await Mediator.Send(new Details.Query { Id = id });
         }
@@ -32,7 +32,7 @@ namespace API.Controllers
         }
 
         [HttpPut("{id}")]
-        [Authorize]
+        //[Authorize(Policy = "IsProjectOwner")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
             command.Id = id;
@@ -40,7 +40,7 @@ namespace API.Controllers
         }
 
         [HttpDelete("{id}")]
-        [Authorize]
+       // [Authorize(Policy = "IsProjectOwner")]
         public async Task<ActionResult<Unit>> Delete(Guid id) => await Mediator.Send(new Delete.Command { Id = id });
       
     }
