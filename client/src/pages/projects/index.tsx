27397c471bf566a52bf98bf4a9079416f4488c2d
moryno <mauricenganga41@gmail.com>
2024-08-17 13:43:27 +0300

    Modify the props

--- client/src/pages/projects/index.tsx
+++ client/src/pages/projects/index.tsx
@@ -2,11 +2,19 @@ import { GroupPage } from '_lib';
 import ProjectForm from './components/ProjectForm';
 import { DomianEnum } from '_constants';
 import { projectColumns } from './columns';
+import { ProjectService } from '_services';
 
 const Projects = () => {
    
   return (
-    <GroupPage title={DomianEnum.PROJECTS} FormComponent={ProjectForm} columns={projectColumns} />
+    <GroupPage 
+      title={DomianEnum.PROJECTS} 
+      FormComponent={ProjectForm} 
+      columns={projectColumns}
+      getAllService={ProjectService.getProjects}
+      getDetailService={ProjectService.getProject}
+      deleteService={ProjectService.deleteProject}
+     />
   )
 }
 
