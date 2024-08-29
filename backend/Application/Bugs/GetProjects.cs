97a24ada17d827f5c2ba44e846dd51ba4e5d8694
moryno <mauricenganga41@gmail.com>
2024-08-29 14:00:47 +0300

    Allow getting all by user

--- backend/Application/Bugs/GetProjects.cs
+++ backend/Application/Bugs/GetProjects.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Interfaces;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -10,20 +11,24 @@ namespace Application.Bugs
         public class Handler : IRequestHandler<Query, List<BugProjectDto>>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _context = context;
+                _userAccessor = userAccessor;
             }
 
             public async Task<List<BugProjectDto>> Handle(Query request, CancellationToken cancellationToken)
             {
                 List<BugProjectDto> projects = await _context.Projects
+                    .Where(x => x.Owner.UserName == _userAccessor.GetCurrentUserName())
                     .Select(project => new BugProjectDto
                     {
                         Id = project.Id,
                         ProjectName =  project.ProjectName
                     })
+                    
                     .ToListAsync(cancellationToken).ConfigureAwait(false);
 
                 return projects;
