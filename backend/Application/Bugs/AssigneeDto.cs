a7376a31bafde90f99d5605939f63402a5befb05
moryno <mauricenganga41@gmail.com>
2024-08-29 10:59:51 +0300

    Done

---
+++ backend/Application/Bugs/AssigneeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Bugs
+{
+    public class AssigneeDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+
+    }
+}
