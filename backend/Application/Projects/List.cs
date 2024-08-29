d8bedaf53b14ffbef1944f89fab00b6d9154fbcf
moryno <mauricenganga41@gmail.com>
2024-08-29 14:00:07 +0300

    Allow getting all by user

--- backend/Application/Projects/List.cs
+++ backend/Application/Projects/List.cs
@@ -1,3 +1,4 @@
+using Application.Interfaces;
 using Application.Projects;
 using AutoMapper;
 using Domain;
@@ -12,16 +13,20 @@ public class List
     {
         private readonly DataContext _context;
         private readonly IMapper _mapper;
+        private readonly IUserAccessor _userAccessor;
 
-        public Handler(DataContext context, IMapper mapper)
+        public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
         {
             _context = context;
             _mapper = mapper;
+            _userAccessor = userAccessor;
         }
 
         public async Task<List<ProjectDto>> Handle(Query request, CancellationToken cancellationToken)
         {
-            var projects = await _context.Projects.ToListAsync();
+            var projects = await _context.Projects
+                .Where(x => x.Owner.UserName == _userAccessor.GetCurrentUserName())
+                .ToListAsync();
 
             return _mapper.Map<List<Project>, List<ProjectDto>>(projects);
         }
