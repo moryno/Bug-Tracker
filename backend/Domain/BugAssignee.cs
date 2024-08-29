9db68cb2f25d5844eeaa66e6dece76bdda0ad8ff
moryno <mauricenganga41@gmail.com>
2024-08-29 11:06:00 +0300

    Done

---
+++ backend/Domain/BugAssignee.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class BugAssignee
+    {
+        public Guid Id { get; set; }
+        public virtual Bug Bug { get; set; } = new Bug();
+        public Guid BugId { get; set; } 
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+    }
+}
