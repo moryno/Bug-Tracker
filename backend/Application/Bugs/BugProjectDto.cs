d85d473e651d18825e04e4e302968188c5b9fd2c
moryno <mauricenganga41@gmail.com>
2024-08-29 11:01:10 +0300

    Done

---
+++ backend/Application/Bugs/BugProjectDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Bugs
+{
+    public  class BugProjectDto
+    {
+        public Guid Id { get; set; }
+        public string ProjectName { get; set; } = string.Empty;
+    }
+}
