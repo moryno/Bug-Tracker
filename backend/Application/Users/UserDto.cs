970533351541230aac06ccc1df4b5ca9863a3f5f
moryno <mauricenganga41@gmail.com>
2024-08-29 11:05:42 +0300

    Done

---
+++ backend/Application/Users/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Users
+{
+    public class UserDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
