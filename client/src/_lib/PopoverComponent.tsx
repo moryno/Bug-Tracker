f5010f878c7c97e821662fe3f95bccf13daac497
moryno <mauricenganga41@gmail.com>
2024-08-29 18:00:51 +0300

    Add user popover to display users

---
+++ client/src/_lib/PopoverComponent.tsx
@@ -0,0 +1,54 @@
+import { Badge, Popover, Table } from "antd"
+import { StyledAssigneeContainer, StyledAssigneeImage, StyledAssigneeName } from "pages/bugs/index.styled"
+import styled from "styled-components"
+
+const PopoverComponent = ({ dataSource, columns}: any) => {
+  return (
+<Popover
+    key={Math.random()}
+    title={null}
+    content={
+      <>
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          size='small'
+          bordered
+          pagination={false}
+          rowKey={Math.random()}
+        />
+      </>
+    }
+  >
+    <style>
+      {`
+
+        .ant-badge-count {
+            pointer-events: none;
+        }
+        `}
+    </style>
+    <StyledDiv>
+      <StyledAssigneeContainer key={dataSource[0].userName}>
+        <StyledAssigneeImage src={dataSource[0].image || "/img/noavatar.jpg"} alt={dataSource[0].fullName} />
+        <StyledAssigneeName>{dataSource[0].fullName}</StyledAssigneeName>
+       </StyledAssigneeContainer>
+    <Badge
+      title=''
+      showZero
+      count={dataSource.length}
+      style={{ backgroundColor: '#2CC8BA' }}
+    />
+    </StyledDiv>
+  </Popover>
+  )
+}
+
+const StyledDiv = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+`
+export default PopoverComponent
