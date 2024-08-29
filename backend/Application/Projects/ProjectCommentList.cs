928375faf99fbba8514d747b9b9b9a02fe1a88a3
moryno <mauricenganga41@gmail.com>
2024-08-29 11:05:09 +0300

    Done

---
+++ backend/Application/Projects/ProjectCommentList.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Projects
+{
+    public class ProjectCommentList
+    {
+        public class Query : IRequest<List<CommentDto>>
+        {
+            public Guid ProjectId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, List<CommentDto>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<List<CommentDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var comments = await _context.ProjectComments
+                    .Where(x => x.Project.Id == request.ProjectId)
+                    .OrderBy(x => x.CreatedAt)
+                    .ProjectTo<CommentDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return comments;
+            }
+        }
+    }
+}
