9b3665384ba9c11e7228c8593493fe232af6de3b
moryno <mauricenganga41@gmail.com>
2024-08-29 11:05:27 +0300

    Done

---
+++ backend/Application/Users/GetAll.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Users
+{
+    public class GetAll
+    {
+        public class Query : IRequest<List<UserDto>> { }
+        public class Handler : IRequestHandler<Query, List<UserDto>>
+        {
+            private readonly DataContext _context;
+            private readonly UserManager<AppUser> _userManager;
+
+            public Handler(DataContext context, UserManager<AppUser> userManager)
+            {
+                _context = context;
+                _userManager = userManager;
+            }
+
+            public async Task<List<UserDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                List<UserDto> users = await _userManager.Users
+                    .Select(user => new UserDto
+                    {
+                        UserName = user.UserName,
+                        FullName = user.DisplayName
+                    })
+                    .ToListAsync(cancellationToken).ConfigureAwait(false);
+
+                return users;
+
+            }
+        }
+    }
+}
