4d034384795ab167f4c825ba8c08ef0e83d68649
moryno <mauricenganga41@gmail.com>
2024-08-29 11:00:06 +0300

    Done

---
+++ backend/Application/Bugs/BugCommentList.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Bugs
+{
+    public class BugCommentList
+    {
+        public class Query : IRequest<List<CommentDto>> 
+        { 
+            public Guid BugId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, List<CommentDto>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<List<CommentDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var comments = await _context.BugComments
+                    .Where(x => x.Bug.Id == request.BugId)
+                    .OrderBy(x => x.CreatedAt)
+                    .ProjectTo<CommentDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return comments;
+            }
+        }
+    }
+}
