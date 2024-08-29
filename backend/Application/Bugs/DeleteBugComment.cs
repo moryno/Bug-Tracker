d5273f833f1a19281e565190b689a454d39e2ee0
moryno <mauricenganga41@gmail.com>
2024-08-29 11:02:46 +0300

    Done

---
+++ backend/Application/Bugs/DeleteBugComment.cs
@@ -0,0 +1,39 @@
+﻿using Application.Errors;
+using Domain;
+using MediatR;
+using Persistence;
+using System.Net;
+
+namespace Application.Bugs
+{
+    public class DeleteBugComment
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                BugComment comment = await _context.BugComments.FindAsync(request.Id);
+                if (comment == null)
+                    throw new RestException(HttpStatusCode.NotFound, new { comment = "Not found." });
+
+                _context.BugComments.Remove(comment);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+                throw new Exception("Problem deleting project");
+            }
+        }
+    }
+}
