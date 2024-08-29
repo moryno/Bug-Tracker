fa515575be5a49d51fc6fb20dc78fa191084b69f
moryno <mauricenganga41@gmail.com>
2024-08-29 11:06:16 +0300

    Done

---
+++ backend/Domain/CommentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
