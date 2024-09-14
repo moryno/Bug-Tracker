62df3c875d6c6415fe77e6b735160add9d262713
moryno <mauricenganga41@gmail.com>
2024-09-14 15:20:18 +0300

    Add common dashboard properties

---
+++ client/src/_constants/common.tsx
@@ -0,0 +1,7 @@
+export const dashCardType = {
+    activeProjects: "Active Projects",
+    completedProjects: "Completed Projects",
+    openTickets: "Open Tickets",
+    unassignedTickets: "Unassigned Tickets",
+    teamMembers: "Team Members",
+}
