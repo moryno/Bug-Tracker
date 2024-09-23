521bdbf6053b338076445cc71649254a3e11b21d
moryno <mauricenganga41@gmail.com>
2024-09-23 08:11:34 +0300

    Done

--- backend/Domain/AppUser.cs
+++ backend/Domain/AppUser.cs
@@ -2,11 +2,15 @@
 
 namespace Domain
 {
-    public class AppUser : IdentityUser
+    public class AppUser : IdentityUser<Guid>
     {
         public string DisplayName { get; set; } = string.Empty;
+        public Guid CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+        public virtual ICollection<AppUserRole> UserRoles { get; set; } = new List<AppUserRole>();
         public virtual ICollection<ProjectComment> ProjectComments { get; set; } = new List<ProjectComment>();
         public virtual ICollection<BugComment> BugComments { get; set; } = new List<BugComment>();
         public virtual ICollection<Project> Projects { get; set; } = new List<Project>();
+        public virtual ICollection<Photo> Photos { get; set; } = new List<Photo>();
     }
 }
