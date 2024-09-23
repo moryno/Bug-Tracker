038fa6d1496bef887d0ed6762427e9867b258f0c
moryno <mauricenganga41@gmail.com>
2024-09-23 08:05:44 +0300

    Done

---
+++ backend/Domain/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual ICollection<AppUser> Users { get; set; } = new List<AppUser>();
+        public virtual ICollection<AppRole> Roles { get; set; } = new List<AppRole>();
+        public virtual ICollection<Project> Projects { get; set; } = new List<Project>();
+        public virtual ICollection<Bug> Bugs { get; set; } = new List<Bug>();
+    }
+}
