f51653e88fb7af003eb362bf549973c2f8b11fbf
moryno <mauricenganga41@gmail.com>
2024-09-23 08:05:58 +0300

    Done

---
+++ backend/Domain/Photo.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class Photo
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public bool IsMain { get; set; }
+    }
+}
