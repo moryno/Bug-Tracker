94d62f00110aa17b802ba897abe7bf7a0f763fe1
moryno <mauricenganga41@gmail.com>
2024-09-23 08:02:09 +0300

    Done

---
+++ backend/API/Controllers/PhotosController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Bugs;
+using Application.Photos;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    public class PhotosController : BaseController
+    {
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<Photo>> Add([FromForm] Add.Command command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<ActionResult<Unit>> Delete(string id) => Ok(await Mediator.Send(new Delete.Command { Id = id }));     
+        [HttpPost("{id}/setMain")]
+        [Authorize]
+        public async Task<ActionResult<Unit>> SetMain(string id) => Ok(await Mediator.Send(new SetMain.Command { Id = id }));
+
+    }
+}
