dc1309dd2d65842155e920e079e375650b83983e
moryno <mauricenganga41@gmail.com>
2024-09-23 08:04:00 +0300

    Done

---
+++ backend/Domain/AppRole.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain
+{
+    public class AppRole : IdentityRole<Guid>
+    {
+        public string CreatedUser { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string UpdatedUser { get; set; } = string.Empty;
+        public DateTime? UpdatedDate { get; set; } 
+        public Guid? CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+        public virtual ICollection<AppUserRole> UserRoles { get; set; } = new List<AppUserRole>();
+
+    }
+}
