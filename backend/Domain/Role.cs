05579d62f4a4f674353cee084e41eae7f4cd14c9
moryno <mauricenganga41@gmail.com>
2024-09-23 08:06:52 +0300

    Done

---
+++ backend/Domain/Role.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string CreatedUser { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string UpdatedUser { get; set; } = string.Empty;
+        public DateTime? UpdatedDate { get; set; }
+        public Guid? CompanyId { get; set; }
+
+    }
+}
---
+++ backend/Domain/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    public class UserProfile
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public Guid CompanyId { get; set; }
+        public ICollection<Photo> photos { get; set; }
+    }
+}
