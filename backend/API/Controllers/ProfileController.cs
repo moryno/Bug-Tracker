c83ca9f7a520df1c5b1a4a3f7a924a7bfd21a4f9
moryno <mauricenganga41@gmail.com>
2024-09-23 08:02:21 +0300

    Done

---
+++ backend/API/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Users;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    public class ProfileController : BaseController
+    {
+        [HttpGet("{userName}")]
+        [Authorize]
+        public async Task<ActionResult<UserProfile>> Get(string userName)
+        {
+            return Ok(await Mediator.Send(new GetProfile.Query { UserName = userName }));
+        }
+    }
+}
