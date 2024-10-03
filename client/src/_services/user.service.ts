8cfe2f7e035893c37f486f600a4f2987de85511a
moryno <mauricenganga41@gmail.com>
2024-10-03 10:40:26 +0300

    Done

--- client/src/_services/user.service.ts
+++ client/src/_services/user.service.ts
@@ -4,7 +4,15 @@ import { request } from "utils";
 const getAllUsers = () => {
   return request.get(`${USER_API}/GetAll`);
 };
+const getTeamMembers = () => {
+  return request.get(USER_API);
+};
+const getProfile = (userName: string) => {
+  return request.get(`profile/${userName}`);
+};
 
 export const UserService = {
   getAllUsers,
+  getTeamMembers,
+  getProfile,
 };
