a3e5ff5d91f5d19286c7e2d383b9914ee5ef926f
moryno <mauricenganga41@gmail.com>
2024-10-03 10:47:14 +0300

    Modify the table of top projects

--- client/src/pages/home/components/TopProjectsComponent.tsx
+++ client/src/pages/home/components/TopProjectsComponent.tsx
@@ -1,12 +1,8 @@
 import { StyledTable } from '_lib';
 import { StyledChartCardHeaderImg, StyledChartCardHeaderTitle, StyledChartCardHeaderWrapper, StyledMiddleChartCardWrapper } from '../index.styled';
-import { useGetAll } from '_hooks';
-import { ProjectService } from '_services';
-import { DomianEnum } from '_constants';
 import { topProjectColumns } from './columns';
 
-const TopProjectsComponent = () => {
-    const { isLoading, error, data } = useGetAll(ProjectService.getProjects, DomianEnum.PROJECTS);
+const TopProjectsComponent = ({ projects } : { projects : any[]}) => {
 
   return (
     <StyledMiddleChartCardWrapper>
@@ -15,9 +11,8 @@ const TopProjectsComponent = () => {
        <StyledChartCardHeaderImg src="/img/moreDark.png" alt="this is an elipses" />
      </StyledChartCardHeaderWrapper> 
           <StyledTable 
-           loading={isLoading} 
            pagination={false}
-           dataSource={data?.data || []} 
+           dataSource={projects || []} 
            columns={topProjectColumns} 
            rowKey={(record: any) => record?.id}
            onRow={(record: any, rowIndex) => {
