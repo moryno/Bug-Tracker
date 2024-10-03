2bac2113701717deb3897a733d65f8f5c0c87ca8
moryno <mauricenganga41@gmail.com>
2024-10-03 10:40:46 +0300

    Done

--- client/src/interfaces/types.ts
+++ client/src/interfaces/types.ts
@@ -1,5 +1,5 @@
 import { MenuProps } from "antd";
-import { IBug, IProject } from "./domain";
+import { IBug, IProject, IRole } from "./domain";
 
 export type drawerType = {
   onClose: () => void;
@@ -13,6 +13,14 @@ export type projectFormType = drawerType & {
 export type bugFormType = drawerType & {
   editedRecord: IBug | null;
 };
+export type roleFormType = drawerType & {
+  editedRecord: IRole | null;
+};
+export type profileType = {
+  profile: ProfileType;
+  onClose: () => void;
+  open: boolean;
+};
 
 export type MenuPropsType = {
   items: MenuProps["items"];
@@ -22,6 +30,10 @@ export type commentType = {
   id?: string;
   description: string;
 };
+export type assignType = {
+  roleIds: string[];
+  userName: string;
+};
 export type InviteUserType = {
   firstName: string;
   lastName: string;
@@ -29,4 +41,20 @@ export type InviteUserType = {
   userName: string;
   role: string;
   message: string;
+};
+
+export type ProfileType = {
+  fullName: string;
+  email: string;
+  userName: string;
+  image?: string;
+  companyName?: string;
+  companyId?: string;
+  photos?: Photo;
+};
+
+type Photo = {
+  Id?: string;
+  url: string;
+  isMain: boolean;
 };
