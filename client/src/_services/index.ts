4b9b99575275049d35033c40dfed8901bbb2bef7
moryno <mauricenganga41@gmail.com>
2024-10-03 10:39:12 +0300

    Done

--- client/src/_services/index.ts
+++ client/src/_services/index.ts
@@ -2,3 +2,5 @@ export * from "./auth.service";
 export * from "./project.service";
 export * from "./bug.service";
 export * from "./user.service";
+export * from "./dashboard.service";
+export * from "./role.service";
