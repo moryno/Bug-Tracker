939a7111a92b7c3321df778004aa96c386da3ebc
moryno <mauricenganga41@gmail.com>
2024-10-03 10:46:10 +0300

    Modify team member component

--- client/src/pages/home/components/TeamMembersComponent.tsx
+++ client/src/pages/home/components/TeamMembersComponent.tsx
@@ -1,143 +1,43 @@
 import { StyledAssigneeImage } from 'pages/projects/index.styled';
-import { StyledChartCardHeaderImg, StyledChartCardHeaderTitle, StyledChartCardHeaderWrapper, StyledMiddleChartCardWrapper, StyledWorkItemInfoDesc, StyledWorkItemInfoTitle, StyledWorkItemInfoWrapper, StyledWorkItemLeftDiv, StyledWorkItemMailIcon, StyledWorkItemProfileIcon, StyledWorkItemRightDiv, StyledWorkItemWrapper } from '../index.styled';
+import { 
+    StyledChartCardHeaderImg, 
+    StyledChartCardHeaderTitle, 
+    StyledChartCardHeaderWrapper, 
+    StyledMiddleChartCardWrapper, 
+    StyledWorkItemInfoDesc, 
+    StyledWorkItemInfoTitle, 
+    StyledWorkItemInfoWrapper, 
+    StyledWorkItemLeftDiv, 
+    StyledWorkItemMailIcon, 
+    StyledWorkItemProfileIcon, 
+    StyledWorkItemRightDiv, 
+    StyledWorkItemWrapper 
+} from '../index.styled';
 
-export const TeamMembersComponent = () => {
+export const TeamMembersComponent = ({ team} : { team : any[]}) => {
   return (
     <StyledMiddleChartCardWrapper className='scrollbar-hide'>
     <StyledChartCardHeaderWrapper className='mb-2'>
      <StyledChartCardHeaderTitle>Team</StyledChartCardHeaderTitle>
       <StyledChartCardHeaderImg src="/img/moreDark.png" alt="this is an elipses" />
     </StyledChartCardHeaderWrapper> 
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
-    <StyledWorkItemWrapper>
-    <StyledWorkItemLeftDiv>
-        <StyledAssigneeImage src={"/img/noavatar.jpg"} />
-        <StyledWorkItemInfoWrapper>
-            <StyledWorkItemInfoTitle>Diana Nyambura</StyledWorkItemInfoTitle>
-            <StyledWorkItemInfoDesc>Project Manager</StyledWorkItemInfoDesc>
-        </StyledWorkItemInfoWrapper>
-    </StyledWorkItemLeftDiv>
-    <StyledWorkItemRightDiv>
-        <StyledWorkItemProfileIcon />
-        <StyledWorkItemMailIcon />
-    </StyledWorkItemRightDiv>
-   </StyledWorkItemWrapper>
+    { team && team?.map(( user : any) => (
+        <StyledWorkItemWrapper key={user?.id}>
+            <StyledWorkItemLeftDiv>
+                <StyledAssigneeImage src={ user?.image || "/img/noavatar.jpg" } />
+                <StyledWorkItemInfoWrapper>
+                    <StyledWorkItemInfoTitle>{ user?.fullName }</StyledWorkItemInfoTitle>
+                    <StyledWorkItemInfoDesc>{ user?.roles?.join(", ") }</StyledWorkItemInfoDesc>
+                </StyledWorkItemInfoWrapper>
+            </StyledWorkItemLeftDiv>
+            <StyledWorkItemRightDiv>
+                <StyledWorkItemProfileIcon />
+                <StyledWorkItemMailIcon />
+            </StyledWorkItemRightDiv>
+        </StyledWorkItemWrapper>
+    ))}
+
+    
    </StyledMiddleChartCardWrapper>
   )
 }
