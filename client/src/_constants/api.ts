296a4c116ac8ebae9c451bb91cfa113b5e8cb8d9
moryno <mauricenganga41@gmail.com>
2024-10-03 10:26:06 +0300

    Done

--- client/src/_constants/api.ts
+++ client/src/_constants/api.ts
@@ -3,12 +3,18 @@ export const LOGIN_API = "/user/login";
 export const REGISTER_API = "/user/register";
 export const LOGOUT_API = "/logout/logout";
 
+// DASHBOARD
+export const DASHBOARD_API = "/dashboard";
+
 // USERS
 export const USER_API = "/user";
+// USERS
+export const PHOTO_API = "/photos";
 
 // PROJECTS
 export const PROJECTS_API = "/projects";
-
+// PROJECTS
+export const ROLES_API = "/roles";
 // BUGS
 export const BUGS_API = "/bugs";
 
