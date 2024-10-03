006b2bce29a9b5a8677570e2a7a6642a635569d7
moryno <mauricenganga41@gmail.com>
2024-10-03 10:45:36 +0300

    Modify bug component

--- client/src/pages/home/components/ResolvedBugsComponent.tsx
+++ client/src/pages/home/components/ResolvedBugsComponent.tsx
@@ -1,7 +1,14 @@
+import { getMonthName } from '_helpers';
 import { StyledChartCardHeaderImg, StyledChartCardHeaderTitle, StyledChartCardHeaderWrapper, StyledMiddleChartCardWrapper } from '../index.styled';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const ResolvedBugsComponent = () => {
+const ResolvedBugsComponent = ({ resolvedSubmittedBugs } : { resolvedSubmittedBugs : any[]}) => {
+
+  const transformedBugs = resolvedSubmittedBugs?.map(bug => ({
+    ...bug,
+    name: getMonthName(bug.name)
+}));
+
   return (
     <StyledMiddleChartCardWrapper>
     <StyledChartCardHeaderWrapper>
@@ -12,7 +19,7 @@ const ResolvedBugsComponent = () => {
         <LineChart
           width={500}
           height={300}
-          data={data}
+          data={transformedBugs}
         >
           <CartesianGrid strokeDasharray="3 3" stroke="#ddd"  />
           <XAxis dataKey="name" axisLine={false}  tick={{ fill: "#d1d5db"}} tickMargin={10} tickLine={false} />
@@ -26,66 +33,5 @@ const ResolvedBugsComponent = () => {
  </StyledMiddleChartCardWrapper>
   )
 }
-const data = [
-    {
-      name: 'Jan',
-      resolved: 4000,
-      submitted: 2400,
-    },
-    {
-      name: 'Feb',
-      resolved: 3000,
-      submitted: 1398,
-    },
-    {
-      name: 'Mar',
-      resolved: 2000,
-      submitted: 9800,
-    },
-    {
-      name: 'April',
-      resolved: 2780,
-      submitted: 3908,
-    },
-    {
-      name: 'May',
-      resolved: 1890,
-      submitted: 4800,
-    },
-    {
-      name: 'Jun',
-      resolved: 2390,
-      submitted: 3800,
-    },
-    {
-      name: 'Jul',
-      resolved: 4300,
-      submitted: 3490,
-    },
-    {
-      name: 'Aug',
-      resolved: 3490,
-      submitted: 4300,
-    },
-    {
-      name: 'Sep',
-      resolved: 2780,
-      submitted: 3908,
-    },
-    {
-      name: 'Oct',
-      resolved: 4300,
-      submitted: 4800,
-    },
-    {
-      name: 'Nov',
-      resolved: 2100,
-      submitted: 2400,
-    },
-    {
-      name: 'Dec',
-      resolved: 4300,
-      submitted: 2400,
-    },
-  ];
+
 export default ResolvedBugsComponent
