463e40c7eda99142d624e5ff5ca264287c1b5f09
moryno <mauricenganga41@gmail.com>
2024-10-03 10:29:00 +0300

    Done

--- client/src/pages/users/columns.tsx
+++ client/src/pages/users/columns.tsx
@@ -1,39 +1,64 @@
-import { IFollowerDto } from "interfaces";
+import { Tag } from "antd";
+import { IUser } from "interfaces";
 import moment from "moment";
-import { StyledAssigneeContainer, StyledAssigneeImage, StyledAssigneeName } from "pages/projects/index.styled";
+import { StyledWorkItemInfoDesc, StyledWorkItemInfoTitle, StyledWorkItemLeftDiv } from "pages/home/index.styled";
+import styled from "styled-components";
 
-const userColumns = [
+export const userColumns = [
     {
-      title: 'Owner',
-      key: 'owner',
-      render: (record: IFollowerDto) => (
-        <StyledAssigneeContainer>
-          <StyledAssigneeImage
-            src={record.image || "/img/noavatar.jpg"}
-            alt={record.fullName || "No name provided"}
+      title: 'Name',
+      key: 'name',
+      render: (record: IUser) => (
+        <StyledWorkItemLeftDiv>
+          <StyledImage
+            src={record?.image || "/img/noavatar.jpg"}
+            alt={record?.fullName || "No name provided"}
           />
-          <StyledAssigneeName>{record.fullName || "Unknown Owner"}</StyledAssigneeName>
-        </StyledAssigneeContainer>
+          <StyledInfoWrapper>
+            <StyledWorkItemInfoTitle>{record?.fullName || "Unknown Owner"}</StyledWorkItemInfoTitle>
+            <StyledWorkItemInfoDesc>{ record?.email }</StyledWorkItemInfoDesc>
+            </StyledInfoWrapper>
+        </StyledWorkItemLeftDiv>
       ),
-      width: 200,
+      width: "30%",
     },
     {
       title: 'Roles',
       key: 'roles',
       dataIndex: 'roles',
       render: (roles: any[]) => roles ? roles.join(', ') : 'No roles assigned',
+      width: "30%",
+    },
+    {
+      title: 'Status',
+      key: 'status',
+      dataIndex: 'status',
+      render: (status: boolean) => <Tag color={status ? "#12CC1B" : "#EF476F"}>{ status ? "Active" : "In Active"}</Tag>,
+      width: "6%",
     },
     {
       title: 'Last Active',
       key: 'lastActive',
       dataIndex: 'lastActive',
       render: (date: Date) => date ? moment(date).format('YYYY-MM-DD HH:mm:ss') : 'No activity recorded',
+      width: "15%",
     },
     {
       title: 'Date Added',
       key: 'dateAdded',
       dataIndex: 'dateAdded',
       render: (date: Date) => moment(date).format('YYYY-MM-DD HH:mm:ss'),
+      width: "15%",
     },
   ];
-  
+  export const StyledImage = styled.img`
+  width: 38px;
+  height: 38px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+export const StyledInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+`;
