3c56a101350c2e7aa4085d748b32ba48bae1d105
moryno <mauricenganga41@gmail.com>
2024-11-09 17:07:24 +0300

    Done

---
+++ backend/Application/Projects/ProjectParams.cs
@@ -0,0 +1,17 @@
+﻿using Application.Core;
+
+namespace Application.Projects
+{
+    public class ProjectParams : PagingParams
+    {
+        public string ProjectName { get; set; } = string.Empty;
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Priority { get; set; } = string.Empty;
+        public string CurrentStatus { get; set; } = string.Empty;
+        public string ProjectStatus { get; set; } = string.Empty;
+        public string Owner { get; set; } = string.Empty;
+        public string ProjectGroup { get; set; } = string.Empty;
+        public string IsActive { get; set; } = string.Empty;
+    }
+}
