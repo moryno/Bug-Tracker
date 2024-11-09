ec9020a240921623570cbd4776f3304684b5f5dd
moryno <mauricenganga41@gmail.com>
2024-11-09 17:03:34 +0300

    Done

---
+++ backend/Application/Bugs/BugParams.cs
@@ -0,0 +1,16 @@
+﻿using Application.Core;
+
+namespace Application.Bugs
+{
+    public class BugParams : PagingParams
+    {
+        public Guid ProjectId { get; set; }
+        public string BugName { get; set; } = string.Empty;
+        public string Severity { get; set; } = string.Empty;
+        public string Classification { get; set; } = string.Empty;
+        public DateTime? DueDate { get; set; }
+        public string BugStatus { get; set; } = string.Empty;
+        public string Assignee { get; set; } = string.Empty;
+        public bool UnAssigneed { get; set; }
+    }
+}
