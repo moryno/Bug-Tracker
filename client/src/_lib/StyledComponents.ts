1e54208d268475fb5b79ee00e70491e4b3c54e49
moryno <mauricenganga41@gmail.com>
2024-11-09 17:55:58 +0300

    Done

--- client/src/_lib/StyledComponents.ts
+++ client/src/_lib/StyledComponents.ts
@@ -1,9 +1,18 @@
 import { defaultTheme } from "_constants";
 import styled from "styled-components";
 import { FaBriefcase, FaBug, FaCheck, FaRegEdit } from "react-icons/fa";
-import { MdOutlineMessage, MdOutlineArrowDropDown } from "react-icons/md";
-import { Button, Card, Col, Modal, Table } from "antd";
+import {
+  MdOutlineMessage,
+  MdOutlineArrowDropDown,
+  MdDelete,
+} from "react-icons/md";
+import { Button, Card, Col, Collapse, Drawer, Modal, Table } from "antd";
 import { IoClose } from "react-icons/io5";
+import { FaFilter, FaPlus } from "react-icons/fa6";
+
+interface DemoTitle {
+  color?: string;
+}
 
 export const StyledUserIcon = styled.span`
   display: flex;
@@ -24,10 +33,12 @@ export const PanelTitle = styled.h3`
   font-size: 15px;
   color: ${defaultTheme.theme.palette.gray[900]};
 `;
-export const StyledComment = styled.span`
+export const StyledComment = styled.div`
   font-size: 14px;
   font-weight: 500;
   color: ${defaultTheme.theme.palette.gray[500]};
+  padding: 0;
+  margin: 0;
 `;
 export const StyledPageTitle = styled.h3`
   padding: 0;
@@ -57,6 +68,7 @@ export const StyledInfoText = styled.p`
   font-size: 15px;
   color: ${defaultTheme.theme.text};
 `;
+
 export const StyledProjectIcon = styled(FaBriefcase).attrs({
   fontSize: 18,
   color: defaultTheme.theme.palette.status.planning,
@@ -132,6 +144,32 @@ export const StyledEditIcon = styled(FaRegEdit).attrs({
 })`
   cursor: pointer;
 `;
+export const StyledFilterIcon = styled(FaFilter).attrs({
+  fontSize: 18,
+  color: defaultTheme.theme.palette.status.onHold,
+})`
+  cursor: pointer;
+`;
+export const StyledNewIcon = styled(FaPlus).attrs({
+  fontSize: 18,
+  color: defaultTheme.theme.palette.primary.main,
+})`
+  cursor: pointer;
+`;
+export const StyledDeleteIcon = styled(MdDelete).attrs({
+  fontSize: 18,
+  color: defaultTheme.theme.palette.status.team,
+})`
+  cursor: pointer;
+`;
+export const StyledIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  padding: 5px;
+  border-radius: 50%;
+`;
 export const StyledModal = styled(Modal)`
   .ant-modal-header {
     // padding: 10px 24px;
@@ -158,4 +196,84 @@ export const StyledPhotoStepLabel = styled.h2`
   font-weight: 500;
   font-size: 17px;
   color: ${defaultTheme.theme.palette.status.completed};
+`;
+export const StyledAppNotificationWrapper = styled.article`
+  flex: 1;
+  height: 90%;
+  overflow-y: auto;
+  background-color: white;
+  padding: 16px 0;
+`;
+export const StyledMediumDrawer = styled(Drawer)`
+  .ant-drawer-body {
+    padding: 0px !important;
+  }
+  .ant-drawer-title {
+    font-size: 14px !important;
+  }
+  .ant-drawer-header {
+    padding: 8px 24px !important;
+  }
+`;
+export const StyledContainerDrawer = styled(Drawer)`
+  .ant-drawer-title {
+    font-size: 14px !important;
+  }
+  .ant-drawer-header {
+    padding: 8px 24px !important;
+  }
+  .ant-drawer-close {
+    align-self: end;
+  }
+`;
+export const StyledFilterCollapseContainer = styled(Collapse)`
+  .ant-collapse-header {
+    padding: 6px 24px !important;
+    display: flex !important;
+    justify-content: space-between !important;
+  }
+  .ant-collapse-content-box {
+    padding-block: 6px !important;
+  }
+`;
+export const StyledDomainFilterContainer = styled.section`
+  height: 100%;
+  overflow-y: auto;
+  background-color: white;
+`;
+export const StyledFormButtonContainer = styled.section`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 10px;
+  margin-top: 20px;
+`;
+export const StyledTextEditorContainer = styled.article`
+  width: 100%;
+  position: relative;
+  margin-bottom: 20px;
+`;
+export const StyledDemoUserWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: space-between;
+`;
+export const StyledDemoUserinfoContainer = styled.div`
+  display: flex;
+  cursor: pointer;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 47%;
+  gap: 5px;
+`;
+export const StyledDemoInfoText = styled.p<DemoTitle>`
+  padding: 0;
+  margin: 0;
+  font-weight: 500;
+  font-size: 15px;
+  color: ${({ theme, color }) => theme.palette.status[color!]};
 `;
