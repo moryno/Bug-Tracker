53b686fca92c9e7e97e49f3f33249cdb13df5092
moryno <mauricenganga41@gmail.com>
2024-11-09 17:08:39 +0300

    Done

---
+++ backend/Application/Users/UserParams.cs
@@ -0,0 +1,11 @@
+﻿using Application.Core;
+
+namespace Application.Users
+{
+    public class UserParams : PagingParams
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
