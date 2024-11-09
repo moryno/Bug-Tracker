4a73cacf6b717b03e12bebc446b81a12bfe26623
moryno <mauricenganga41@gmail.com>
2024-11-09 17:07:49 +0300

    Done

---
+++ backend/Application/Projects/ProjectSearchDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Projects
+{
+    public class ProjectSearchDto
+    {
+        public Guid Id { get; set; }
+        public string ProjectName { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string CurrentStatus { get; set; } = string.Empty;
+        public string Priority { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Count {  get; set; } = 0;
+    }
+}
