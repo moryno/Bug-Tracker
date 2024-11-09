243bd4989f9ca6bb7b1ca44e6d4b75757760be17
moryno <mauricenganga41@gmail.com>
2024-11-09 17:09:48 +0300

    Done

---
+++ backend/Domain/CalendarEventDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class CalendarEventDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
