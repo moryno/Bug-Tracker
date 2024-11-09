c007bfa5b68a20914a50eae1afe2bbad60ee7c7e
moryno <mauricenganga41@gmail.com>
2024-11-09 17:12:41 +0300

    Done

---
+++ backend/Domain/NotificationDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain
+{
+    public class NotificationDto
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool IsRead { get; set; } = false;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ReadAt { get; set; }
+        public string EventTitle { get; set; } = string.Empty;
+        public DateTime EventStartDate { get; set; }
+        public Guid EventId { get; set; }
+        public string RecepientName { get; set; } = string.Empty;
+        public string RecepientUserName { get; set; } = string.Empty;
+        public string SenderName { get; set; } = string.Empty;
+        public string SenderEmail { get; set; } = string.Empty;
+        public string SenderImage { get; set; } = string.Empty;
+        public string SenderUserName { get; set; } = string.Empty;
+
+    }
+}
