99572dff9a46641d47080302dd2b090e38cd3952
moryno <mauricenganga41@gmail.com>
2024-11-09 18:13:29 +0300

    Done

--- backend/Domain/AppUser.cs
+++ backend/Domain/AppUser.cs
@@ -9,11 +9,15 @@ namespace Domain
         public bool Status { get; set; }
         public DateTime LastActive { get; set; }
         public DateTime DateAdded { get; set; }
-        public virtual Company Company { get; set; }
-        public virtual ICollection<AppUserRole> UserRoles { get; set; }
-        public virtual ICollection<ProjectComment> ProjectComments { get; set; }
-        public virtual ICollection<BugComment> BugComments { get; set; }
-        public virtual ICollection<Project> Projects { get; set; }
-        public virtual ICollection<Photo> Photos { get; set; }
+        public virtual Company? Company { get; set; }
+        public virtual ICollection<AppUserRole>? UserRoles { get; set; }
+        public virtual ICollection<ProjectComment>? ProjectComments { get; set; }
+        public virtual ICollection<BugComment>? BugComments { get; set; }
+        public virtual ICollection<Project>? Projects { get; set; }
+        public virtual ICollection<Photo>? Photos { get; set; }
+        public virtual ICollection<EventAttendee> Events { get; set; } = new List<EventAttendee>();
+        public virtual ICollection<EventComment> EventComments { get; set; } = new List<EventComment>();
+        public virtual ICollection<Notification> NotificationsSent { get; set; } = new List<Notification>();
+        public virtual ICollection<Notification> NotificationsReceived { get; set; } = new List<Notification>();
     }
 }
