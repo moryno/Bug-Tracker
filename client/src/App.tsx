81c1eca27c7b30262848d47c2c28a61c103c5c31
moryno <mauricenganga41@gmail.com>
2024-11-09 18:32:39 +0300

    Add font family to the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,9 +8,19 @@ import { PersistGate } from "redux-persist/integration/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import "@fontsource/inter"; 
+import "@fontsource/inter/200.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 
 function App() {
   const queryClient = new QueryClient();
+  
 
   return (
   <Provider store={store}>
