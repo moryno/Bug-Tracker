c520e65f73aa36b0095362395cbea50d5a8a848e
moryno <mauricenganga41@gmail.com>
2024-11-09 18:11:00 +0300

    Done

--- backend/Domain/DashboardGridStatsResult.cs
+++ backend/Domain/DashboardGridStatsResult.cs
@@ -2,22 +2,22 @@
 {
     public class DashboardGridStatsResult
     {
-        public List<TopProjectDto> TopProjects { get; set; }
+        public List<TopProjectDto>? TopProjects { get; set; }
         public List<UserDto> Users { get; set; } = new List<UserDto>();
-        public List<Dictionary<string, int>> ResolvedSubmittedBugs { get; set; } = new List<Dictionary<string, int>>();
+        public List<Dictionary<string, int>>? ResolvedSubmittedBugs { get; set; }
 
     }
 
     public class TopProjectDto
     {
         public Guid Id { get; set; }
-        public string Priority { get; set; }
+        public string? Priority { get; set; }
         public DateTime EndDate { get; set; }
-        public string Description { get; set; }
-        public string ProjectName { get; set; }
+        public string? Description { get; set; }
+        public string? ProjectName { get; set; }
         public double ProjectStatus { get; set; }
-        public string ProjectOwnerFullName { get; set; }
-        public string ProjectOwnerImage { get; set; }
+        public string? ProjectOwnerFullName { get; set; }
+        public string? ProjectOwnerImage { get; set; }
     }
 
 }
