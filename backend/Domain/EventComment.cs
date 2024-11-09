410ca194563adb8b97346f1ed083cdb81a64d336
moryno <mauricenganga41@gmail.com>
2024-11-09 17:10:44 +0300

    Done

---
+++ backend/Domain/EventComment.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class EventComment
+    {
+        public Guid Id { get; set; }
+        public virtual AppUser? Author { get; set; }
+        public virtual Event? Event { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
