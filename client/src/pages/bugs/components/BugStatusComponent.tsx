4a0ad98cbef580f80a7ce5a9d2b0a9eb189c3caa
moryno <mauricenganga41@gmail.com>
2024-11-09 17:31:25 +0300

    Done

--- client/src/pages/bugs/components/BugStatusComponent.tsx
+++ client/src/pages/bugs/components/BugStatusComponent.tsx
@@ -1,23 +1,13 @@
-import {
-    StyledBugStatusContainer,
-    StyledBugStatusText,
-  StyledBugStatusTitle,
-  StyledStatusDot,
-} from "../index.styled";
-import {
-    StyledCardWrapper,
-  StyledDropDownIcon,
-
-} from "_lib";
+import { Tag } from "antd";
+import { StyledBugStatusContainer, StyledBugStatusTitle } from "../index.styled";
+import { StyledCardWrapper } from "_lib";
 
 
-const BugStatusComponent = ({ status} : { status: string}) => {
+const BugStatusComponent = ({ status } : { status: string}) => {
   return (
     <StyledCardWrapper>
        <StyledBugStatusContainer>
-        <StyledStatusDot />
-        <StyledBugStatusText>{status}</StyledBugStatusText>
-        <StyledDropDownIcon />
+       <Tag color={`${status === "Open" ? "#2CC8BA" : status === "InProgress" ? "#08AEEA" : "#4ED3E5"}`}>{status}</Tag> 
       </StyledBugStatusContainer>
       <StyledBugStatusTitle>CURRENT STATUS</StyledBugStatusTitle>
     </StyledCardWrapper>
