5438a045c1e494ec8ca5cb9ca5d07c070f7b44bb
moryno <mauricenganga41@gmail.com>
2024-11-09 17:20:41 +0300

    Done

--- client/src/pages/projects/components/ProjectDetailHeader.tsx
+++ client/src/pages/projects/components/ProjectDetailHeader.tsx
@@ -1,15 +1,73 @@
+import { defaultTheme, PROJECT_ROUTE } from "_constants";
 import {
+  StyledDetailCloseIcon,
+  StyledDetailEditIcon,
+  StyledDetailMoreIcon,
   StyledProgress,
   StyledTitleContainer,
   StyledTopContainer,
+  StyledTopIconWrapper,
+  StyledTopWrapper,
+  StyleTopLeftWrapper,
+  StyleTopRightWrapper,
 } from "../index.styled";
-import { DetailInfoPopover, StyledCommentIcon, StyledPageTitle, StyledTagContainer } from "_lib";
-import { Tag } from "antd";
+import { DetailInfoPopover, StyledCommentIcon, StyledPageTitle, StyledProjectIcon, StyledTagContainer } from "_lib";
+import { Divider, Dropdown, MenuProps, Tag } from "antd";
 import { IProject } from "interfaces";
+import { StyledDeleteIcon } from "pages/bugs/index.styled";
+import { StyledActionTitle, StyledMenuItemActions } from "pages/users/index.styled";
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { StyledSearchProjectIcon } from "_lib/AppSearchComponent/index.styled";
 
-const ProjectDetailHeader = ({ project } : { project : IProject }) => {
+const ProjectDetailHeader = ({ project, onEdit, deleteProject } : { project : IProject, onEdit: () => void, deleteProject: (project: IProject) => void }) => {
+  const status = project.currentStatus!;
+  const navigate = useNavigate();
+
+  const onClose = useCallback(() => {
+    if (window.history?.length && window.history.length > 1) {
+      navigate(-1);
+   } else {
+      navigate(PROJECT_ROUTE, { replace: true });
+   }
+  }, [navigate])
+
+  const menuItems = useCallback((): MenuProps['items'] => {
+
+    return [
+      {
+        key: 'edit',
+        label: (
+          <StyledMenuItemActions>
+            <StyledDetailEditIcon />
+            <StyledActionTitle>Edit Project</StyledActionTitle>
+          </StyledMenuItemActions>
+        ),
+        onClick: onEdit,
+      },
+      
+      {
+        key: 'delete',
+        label: (
+          <StyledMenuItemActions>
+            <StyledDeleteIcon />
+            <StyledActionTitle style={{ color: defaultTheme.theme.palette.status.team}}>Delete Project</StyledActionTitle>
+          </StyledMenuItemActions>
+        ),
+        onClick: (e) => deleteProject(project),
+      },
+    ];
+  }, [deleteProject, onEdit, project]);
+
   return (
     <StyledTopContainer>
+      <Tag style={{ marginBottom: 5 }}>
+        <StyledTopIconWrapper>
+        <StyledSearchProjectIcon /> Project
+        </StyledTopIconWrapper>
+      </Tag>
+      <StyledTopWrapper>
+    <StyleTopLeftWrapper>
       <StyledProgress
         size="small"
         type="circle"
@@ -19,11 +77,23 @@ const ProjectDetailHeader = ({ project } : { project : IProject }) => {
       <StyledTitleContainer>
         <StyledPageTitle>{ project?.projectName }</StyledPageTitle>
         <StyledTagContainer>
-          <Tag>Completed</Tag> <span>|</span>
-          <StyledCommentIcon /> <span>|</span>
+          <Tag color={`${
+            status === "Delayed"  ? "#C5A070" : status === "Active" ? "#2CC8BA" : status === "InProgress" ? "#08AEEA" : status === "OnTrack" ? "#74CB80" : status === "InTesting" ? "#F6A96D" : "#FBC11E"
+          }`}>{status}</Tag> 
+          <Divider type="vertical" />
+          <StyledCommentIcon /> 
+          <Divider type="vertical" />
           <DetailInfoPopover info={project} isOwner={true} />
         </StyledTagContainer>
       </StyledTitleContainer>
+      </StyleTopLeftWrapper>
+      <StyleTopRightWrapper>
+        <Dropdown menu={{ items: menuItems() }}>
+         <StyledDetailMoreIcon />
+        </Dropdown>
+        <StyledDetailCloseIcon onClick={onClose} />
+      </StyleTopRightWrapper>
+      </StyledTopWrapper>
     </StyledTopContainer>
   );
 };
