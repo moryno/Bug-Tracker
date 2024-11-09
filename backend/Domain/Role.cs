cd34f48ea72ae375e421165cbe52f89a3e7505e4
moryno <mauricenganga41@gmail.com>
2024-11-09 18:09:26 +0300

    Done

--- backend/Domain/Role.cs
+++ backend/Domain/Role.cs
@@ -3,7 +3,7 @@
     public class Role
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string Description { get; set; } = string.Empty;
         public string CreatedUser { get; set; } = string.Empty;
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
