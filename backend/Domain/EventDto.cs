8cf123ff352eb78883fd8fc42a6c7e93c6935bf8
moryno <mauricenganga41@gmail.com>
2024-11-09 17:11:30 +0300

    Done

---
+++ backend/Domain/EventDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain
+{
+    public class EventDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Location { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string InitialComment { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string CreatedUser { get; set; } = string.Empty;
+
+    }
+}
