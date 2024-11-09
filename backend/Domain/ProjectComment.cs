60adc893827eb3729727c3c302e428b12c7b20b0
moryno <mauricenganga41@gmail.com>
2024-11-09 18:10:00 +0300

    Done

--- backend/Domain/ProjectComment.cs
+++ backend/Domain/ProjectComment.cs
@@ -3,8 +3,8 @@
     public class ProjectComment
     {
         public Guid Id { get; set; }
-        public virtual AppUser Author { get; set; }
-        public virtual Project Project { get; set; }
+        public virtual AppUser? Author { get; set; }
+        public virtual Project? Project { get; set; }
         public string Description { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
     }
