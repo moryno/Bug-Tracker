a27b6143c63c44b384e2ed6c4e48ce9b7e795ee0
moryno <mauricenganga41@gmail.com>
2024-11-09 17:29:43 +0300

    Done

--- client/src/pages/home/components/Card.tsx
+++ client/src/pages/home/components/Card.tsx
@@ -1,9 +1,12 @@
 import { StyledCardIcon, StyledHomeCard, StyledHomeCardCount, StyledHomeCardDiv, StyledHomeCardTitle } from "../index.styled"
 import { useCallback } from "react";
-import { dashCardType } from "_constants";
+import { BUG_ROUTE, dashCardType, PROJECT_ROUTE, USER_ROUTE } from "_constants";
+import { useNavigate } from "react-router-dom";
+import { setParams } from "_helpers";
 
 
-const Card = ({ title, count } : { title: string, count: number }) => {
+const Card = ({ title, count, type } : { title: string, count: number, type: string }) => {
+  const navigate = useNavigate();
     
     const dashCardIcons = useCallback((e: string) => {
         switch (e) {
@@ -21,8 +24,36 @@ const Card = ({ title, count } : { title: string, count: number }) => {
             break;
         }
       }, []);
+    
+    const onNavigate = useCallback(() => {
+      if(type === "project"){
+        if( title === "Active Projects")
+        {
+          let url = setParams("isActive", "True");
+          navigate(`${PROJECT_ROUTE}?${url}`)
+        } 
+        else{
+          let url = setParams("projectStatus", "Completed");
+          navigate(`${PROJECT_ROUTE}?${url}`)
+        }   
+      }
+      else if (type === "bug"){
+        if( title === "Open Tickets")
+          {
+            let url = setParams("bugStatus", "Open");
+            navigate(`${BUG_ROUTE}?${url}`)
+          } 
+          else{
+            let url = setParams("unAssigneed", "True");
+            navigate(`${BUG_ROUTE}?${url}`)
+          } 
+      }else {
+        navigate(USER_ROUTE)
+      }
+    }, [navigate, title, type]);
+
   return (
-    <StyledHomeCard bg={title}>
+    <StyledHomeCard onClick={onNavigate} bg={title}>
         <StyledHomeCardDiv>
             <StyledHomeCardCount>{ count }</StyledHomeCardCount>
             <StyledCardIcon src={dashCardIcons(title)} />
