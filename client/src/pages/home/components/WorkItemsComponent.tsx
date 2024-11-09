13c88e7a507094f3ee8a3edbee4a078580d81d80
moryno <mauricenganga41@gmail.com>
2024-11-09 17:26:09 +0300

    Done

--- client/src/pages/home/components/WorkItemsComponent.tsx
+++ client/src/pages/home/components/WorkItemsComponent.tsx
@@ -1,12 +1,32 @@
 import moment from 'moment';
-import { StyledChartCardHeaderImg, StyledChartCardHeaderTitle, StyledChartCardHeaderWrapper, StyledChartCardWrapper, StyledWorkItemBugIcon, StyledWorkItemDueDate, StyledWorkItemInfoDesc, StyledWorkItemInfoTitle, StyledWorkItemInfoWrapper, StyledWorkItemLeftDiv, StyledWorkItemWrapper } from '../index.styled';
+import { StyledChartCardHeaderImg, 
+  StyledChartCardHeaderTitle, 
+  StyledChartCardHeaderWrapper, 
+  StyledChartCardWrapper, 
+  StyledWorkItemBugIcon, 
+  StyledWorkItemDueDate, 
+  StyledWorkItemInfoDesc, 
+  StyledWorkItemInfoTitle, 
+  StyledWorkItemInfoWrapper, 
+  StyledWorkItemLeftDiv, 
+  StyledWorkItemWrapper
+ } from '../index.styled';
+import { useCallback } from 'react';
+import { BUG_ROUTE } from '_constants';
+import { useNavigate } from 'react-router-dom';
 
 const WorkItemsComponent = ({ workItems } : { workItems : any[]}) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = useCallback(() => {
+    navigate(BUG_ROUTE)
+  }, [navigate])
+  
   return (
     <StyledChartCardWrapper className='scrollbar-hide'>
     <StyledChartCardHeaderWrapper  className='mb-2'>
      <StyledChartCardHeaderTitle>My Work Items Due This Week</StyledChartCardHeaderTitle>
-     <StyledChartCardHeaderImg src="/img/moreDark.png" alt="this is an elipses" />
+     <StyledChartCardHeaderImg onClick={handleNavigate} src="/img/moreDark.png" alt="this is an elipses" />
    </StyledChartCardHeaderWrapper> 
    { workItems && workItems?.map(( bug : any) => (
       <StyledWorkItemWrapper key={bug?.id}>
