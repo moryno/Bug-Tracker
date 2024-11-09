ccda28c0fdd0db3ecf0fe7b77ab6fe89dc01eef9
moryno <mauricenganga41@gmail.com>
2024-11-09 17:05:36 +0300

    Done

---
+++ backend/Application/Core/CreateEntityEmailService.cs
@@ -0,0 +1,62 @@
+﻿using Application.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Core
+{
+    public class CreateEntityEmailService
+    {
+        private readonly IEmailSender _emailSender;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CreateEntityEmailService(IEmailSender emailSender, IHttpContextAccessor httpContextAccessor)
+        {
+            _emailSender = emailSender;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task SendEntityCreationAsync(IEntity entity, IEnumerable<AppUser>? assignees, AppUser user)
+        {
+            string origin = _httpContextAccessor.HttpContext.Request.Headers["origin"].ToString();
+            string entityUrl = entity is Project
+                         ? $"{origin}/projects/{entity.Id}"
+                         : $"{origin}/bugs/{entity.Id}";
+
+            foreach (var assignee in assignees)
+            {
+                string entityName = entity is Project project
+                                        ? project.ProjectName
+                                    : ((Bug)entity).BugName;
+                var entityStartDate = entity is Project proj
+                                        ? proj.StartDate
+                                    : ((Bug)entity).CreatedDate; 
+                var entityEndDate = entity is Project pro
+                                        ? pro.StartDate
+                                    : ((Bug)entity).CreatedDate;
+                var entityType = entity is Project 
+                                    ? "project" : "bug";
+                var description = entity is Project p
+                                    ? p.Description : ((Bug)entity).Description;
+
+                var mailSettings = new MailSettings
+                {
+                    
+                    ToEmail = assignee.Email,
+                    Subject = $"You're invited to {entityName}",
+                    Body = EntityEmailHtmlHelper.GetEntityEmailHtmlHelper(
+                    entityName,
+                    entityType,
+                    assignee.DisplayName,
+                    entityUrl,
+                    entityStartDate.ToString("MM-dd-yyyy hh:mm tt"),
+                    entityEndDate.ToString("MM-dd-yyyy hh:mm tt"),
+                    DateTime.UtcNow.ToString("MM-dd-yyyy hh:mm tt"),
+                    user.DisplayName,
+                    description
+                    ),
+                };
+
+                await _emailSender.SendMailAsync(mailSettings);
+            }
+        }
+    }
+}
---
+++ backend/Application/Core/EntityEmailHtmlHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace Application.Core
+{
+    public static class EntityEmailHtmlHelper
+    {
+        public static string GetEntityEmailHtmlHelper(
+            string entityName,
+            string entityType,
+            string assigneeName,
+            string redirectUrl,
+            string entityStartDate,
+            string entityEndDate,
+            string entityCreatedDate,
+            string createdBy,
+            string description
+        )
+        {
+            string response = "<div style=\"background-color:#F8F8F8;padding:30px 60px;\">";
+
+            // Event Title and Location
+            response += "<div>";
+            response += $"<p>Hi {assigneeName},</p>";
+            response += $"<p>You have been invited to participate in <a href='{redirectUrl}' style=\"color:#12CC1B;\">{entityName}</a></p>";
+            response += "</div>";
+
+            response += "<div style=\"background-color:#fff;padding:10px 20px 30px;\">";
+
+            // Event reminder message
+            response += "<div style=\"padding:5px 0px;border-bottom:1px #EAECF0 solid;font-size:16px;\">";
+            response += $"Just a friendly reminder to let you know about the {entityType} scheduled from {entityStartDate} to {entityEndDate}";
+            response += "</div>";
+
+            // Organizer details
+            response += "<div style=\"display:flex;margin:15px 0px\">";
+            response += "<span style=\"width:200px;font-size:15px;\">Created by</span>";
+            response += $"<span style=\"font-size:15px;font-weight:500;\">: {createdBy}</span>";
+            response += "</div>";
+
+            // Start date
+            response += "<div style=\"display:flex;margin-bottom:15px\">";
+            response += "<span style=\"width:200px;font-size:15px;\">Starts At</span>";
+            response += $"<span style=\"font-size:15px;font-weight:500;\">: {entityStartDate}</span>";
+            response += "</div>";
+
+            // End date
+            response += "<div style=\"display:flex;margin-bottom:15px\">";
+            response += "<span style=\"width:200px;font-size:15px;\">Ends At</span>";
+            response += $"<span style=\"font-size:15px;font-weight:500;\">: {entityEndDate}</span>";
+            response += "</div>";
+
+            // Event comments
+            response += "<div style=\"padding:5px 0px 20px;border-bottom:1px #EAECF0 solid;\">";
+            response += "<h3>Description(s)</h3>";
+            response += $"<p>{description}</p>";
+            response += $"<span style=\"color:#667085;font-weight:500;\">by {createdBy} on {entityCreatedDate}</span>";
+            response += "</div>";
+
+            response += "</div>";
+
+            // Call to action button
+            response += "<div style=\"padding:30px 0;\">";
+            response += $"<a href='{redirectUrl}' style='padding:10px 20px;color:#fff;background-color:#12CC1B;'>View more details</a>";
+            response += "</div>";
+
+            response += "</div>";
+
+            return response;
+        }
+    }
+}
