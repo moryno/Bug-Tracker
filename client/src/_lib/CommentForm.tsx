e1d286304387b8527c88c8289489b92495e18cda
moryno <mauricenganga41@gmail.com>
2024-11-09 17:55:31 +0300

    Done

--- client/src/_lib/CommentForm.tsx
+++ client/src/_lib/CommentForm.tsx
@@ -1,75 +1,51 @@
-import React, { useCallback, useState } from 'react'
+import  { useCallback, useContext } from 'react'
+import "react-quill/dist/quill.snow.css"
 import { 
    StyledAttachButton,
    StyledAttachContainer, 
    StyledCommentButtonContainer, 
    StyledCommentContainer, 
    StyledCommentWrapper, 
-   StyledDateText, 
-   StyledTextArea } from './index.styled'
-import { StyledUserIcon } from './StyledComponents'
-import { Form, Row } from 'antd'
+ } from './index.styled'
+import { StyledTextEditorContainer, StyledUserIcon } from './StyledComponents'
+import { Row } from 'antd'
 import ContainerButton from './ContainerButton'
-import { commentType } from 'interfaces'
-interface IProps{
-  onAddComment: (comment: commentType| null) => void;
-  loading: boolean;
-  editedComment: commentType | null;
-  statusMode: string;
-}
+import { useAuthUser } from '_hooks'
+import { getAbbreviation } from '_helpers'
+import { CommentContext } from '_context'
+import ReactQuill from 'react-quill'
 
-const initialData:commentType = {
-  description: ""
-}
 
-const CommentForm:React.FC<IProps> = ({ onAddComment, loading, editedComment, statusMode }) => {
+const CommentForm = () => {
+  const { onAddComment, isLoading, comment, handleChange } = useContext(CommentContext)
+  const { user } = useAuthUser();
     // const [open, setOpen] = useState(false);
-    const [description, setDescription] = useState<commentType | null>(statusMode === "CreateMode" ? initialData : editedComment);
     const onFocus = useCallback(() => {
         // setOpen(prev => !prev)
     }, []);
 
-    const handleValueChange = useCallback((e: any) => {
-      setDescription(prev => (
-        { ...prev, ...e }
-      ));
-    },[]);
     const addComment = useCallback(() => {
-        onAddComment(description)
-    }, [description, onAddComment]);
+        onAddComment()
+    }, [onAddComment]);
 
   return (
     <StyledCommentContainer>
-      <StyledUserIcon>MN</StyledUserIcon>
+      <StyledUserIcon>{getAbbreviation(user?.fullName!)}</StyledUserIcon>
         <StyledCommentWrapper>
-         <Row gutter={16}>
-            <Form 
-             initialValues={description || initialData}
-             onValuesChange={handleValueChange}
-            >
-              <Form.Item
-              style={{ width: "100%"}}
-                name="description"
-                rules={[
-                    {
-                    required: true,
-                    message: 'Type a comment...',
-                    },
-                ]}
-                >
-              <StyledTextArea onFocus={onFocus} rows={4} cols={240} placeholder="Please enter comment" />
-            </Form.Item>
-            </Form>
+         <Row>
+            <StyledTextEditorContainer >
+            <ReactQuill placeholder='Please enter comment' theme="snow" onChange={handleChange} value={comment} />
+            </StyledTextEditorContainer>
           </Row>
           {/* {open &&  */}
           <Row gutter={16}>
-            <StyledAttachContainer span={12}>
+            <StyledAttachContainer span={6}>
               <StyledAttachButton />
-              <StyledDateText>Attach Files</StyledDateText>
+              {/* <StyledDateText>Attach Files</StyledDateText> */}
             </StyledAttachContainer>
-            <StyledCommentButtonContainer span={12}>
-              <ContainerButton loading={loading} title="Add Comment" type="primary" size='middle' onClick={addComment} />
-              <ContainerButton disabled={loading} title="Cancel" type="dashed" size='middle' />
+            <StyledCommentButtonContainer span={18}>
+              <ContainerButton loading={isLoading} title="Add Comment" type="primary" size='middle' onClick={addComment} />
+              <ContainerButton disabled={isLoading} title="Cancel" type="dashed" size='middle' />
             </StyledCommentButtonContainer>
           </Row>
          {/* } */}
