4e9b5eb1161d59bd48ff6271a8fd9c4098ac7f2f
moryno <mauricenganga41@gmail.com>
2024-11-09 17:11:10 +0300

    Done

---
+++ backend/Domain/EventDetailDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain
+{
+    public class EventDetailDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Location { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string InitialComment { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string CreatedUser { get; set; } = string.Empty;
+        public Guid ProjectId { get; set; }
+        public string ProjectName { get; set; } = string.Empty;
+        public ICollection<AssigneeDto> Attendees { get; set; } = new List<AssigneeDto>();
+    }
+}
