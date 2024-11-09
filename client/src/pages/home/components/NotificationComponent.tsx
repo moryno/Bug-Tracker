1b8de6431843e3e34efe8cb76ff0ddb5eff076b5
moryno <mauricenganga41@gmail.com>
2024-11-09 17:29:23 +0300

    Done

--- client/src/pages/home/components/NotificationComponent.tsx
+++ client/src/pages/home/components/NotificationComponent.tsx
@@ -10,51 +10,43 @@ import {
     StyledNotificationHeaderTitle,
     StyledNotificationWrapper
      } from '../index.styled';
+import { EVENT_ROUTE } from '_constants';
+import { Spin } from 'antd';
+import { INotification } from 'interfaces';
+import moment from 'moment';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RootState } from '_redux';
+import { useSelector } from 'react-redux';
 
 const NotificationComponent = () => {
+    const { notifications, isFetching } = useSelector((store: RootState) => store.notification);
+    const navigate = useNavigate();
+
+    const handleNavigate = useCallback(() => {
+      navigate(EVENT_ROUTE)
+    }, [navigate])
+
   return (
+    isFetching ? <Spin /> :
     <StyledMiddleChartCardWrapper className='scrollbar-hide'>
      <StyledChartCardHeaderWrapper className='mb-5'>
         <StyledChartCardHeaderTitle>Notifications</StyledChartCardHeaderTitle>
-        <StyledChartCardHeaderImg src="/img/moreDark.png" alt="this is an elipses" />
+        <StyledChartCardHeaderImg onClick={handleNavigate} src="/img/moreDark.png" alt="this is an elipses" />
     </StyledChartCardHeaderWrapper> 
     <StyledNotificationContainer>
-        <StyledNotificationWrapper bg={"blue"}>
-            <StyledNotificationHeaderDiv className="flex items-center justify-between">
-                <StyledNotificationHeaderTitle>Midterm Exams</StyledNotificationHeaderTitle>
-                <StyledNotificationHeaderDate>2024-10-10</StyledNotificationHeaderDate>
-            </StyledNotificationHeaderDiv>
-            <StyledNotificationDescription>
-            Midterm exams will start next week. Please ensure you are prepared and have all necessary materials. 
-            </StyledNotificationDescription>
-        </StyledNotificationWrapper>
-        <StyledNotificationWrapper bg={"green"}>
-            <StyledNotificationHeaderDiv className="flex items-center justify-between">
-                <StyledNotificationHeaderTitle>School Closed</StyledNotificationHeaderTitle>
-                <StyledNotificationHeaderDate>2024-10-14</StyledNotificationHeaderDate>
-            </StyledNotificationHeaderDiv>
-            <StyledNotificationDescription>
-             The school will be closed on Friday for a public holiday. Classes will resume on Monday. 
-            </StyledNotificationDescription>
-        </StyledNotificationWrapper>
-        <StyledNotificationWrapper bg={"greenLight"}>
-            <StyledNotificationHeaderDiv className="flex items-center justify-between">
-                <StyledNotificationHeaderTitle>Field Trip</StyledNotificationHeaderTitle>
-                <StyledNotificationHeaderDate>2024-11-01</StyledNotificationHeaderDate>
-            </StyledNotificationHeaderDiv>
-            <StyledNotificationDescription>
-            The annual field trip is scheduled for next month. Permission slips will be distributed soon.
-            </StyledNotificationDescription>
-        </StyledNotificationWrapper>
-        <StyledNotificationWrapper bg={"red"}>
-            <StyledNotificationHeaderDiv className="flex items-center justify-between">
-                <StyledNotificationHeaderTitle>Field Trip</StyledNotificationHeaderTitle>
-                <StyledNotificationHeaderDate>2024-11-01</StyledNotificationHeaderDate>
-            </StyledNotificationHeaderDiv>
-            <StyledNotificationDescription>
-            The annual field trip is scheduled for next month. Permission slips will be distributed soon.
-            </StyledNotificationDescription>
-        </StyledNotificationWrapper>
+    {notifications?.map((notification : INotification, index: number) => (
+        <StyledNotificationWrapper key={notification.id} bg={index.toString()}>
+        <StyledNotificationHeaderDiv className="flex items-center justify-between">
+            <StyledNotificationHeaderTitle>{notification.eventTitle}</StyledNotificationHeaderTitle>
+            <StyledNotificationHeaderDate>{moment(notification?.eventStartDate).format("YYYY-MM-DD")}</StyledNotificationHeaderDate>
+        </StyledNotificationHeaderDiv>
+        <StyledNotificationDescription>
+         {notification.message}
+        </StyledNotificationDescription>
+    </StyledNotificationWrapper>
+    ))}
+        
     </StyledNotificationContainer>
       </StyledMiddleChartCardWrapper>
   )
