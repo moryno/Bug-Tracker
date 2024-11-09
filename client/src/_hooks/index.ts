0bf4d7333f2c18593f6eadcc5127c23edd4b34d9
moryno <mauricenganga41@gmail.com>
2024-11-09 18:00:28 +0300

    Done

--- client/src/_hooks/index.ts
+++ client/src/_hooks/index.ts
@@ -2,3 +2,6 @@ export * from "./useAuthUser";
 export * from "./useScroll";
 export * from "./useRequestService";
 export { default as useQuery } from "./useQuery";
+export { default as useNotificationHub } from "./useNotificationHub";
+export { default as useDidUpdate } from "./useDidUpdate";
+export { default as useModal } from "./useModal";
