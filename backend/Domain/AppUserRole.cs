80e87b77d5c1caec637b730215f0204add77de8b
moryno <mauricenganga41@gmail.com>
2024-11-09 18:13:02 +0300

    Done

--- backend/Domain/AppUserRole.cs
+++ backend/Domain/AppUserRole.cs
@@ -4,7 +4,7 @@ namespace Domain
 {
     public class AppUserRole : IdentityUserRole<Guid>
     {
-        public virtual AppUser User { get; set; }
-        public virtual AppRole Role { get; set; }
+        public virtual AppUser? User { get; set; }
+        public virtual AppRole? Role { get; set; }
     }
 }
