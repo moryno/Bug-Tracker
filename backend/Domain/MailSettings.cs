3a3dda501c9b0e92dd1c9919513f166ced6ad411
moryno <mauricenganga41@gmail.com>
2024-11-09 18:10:36 +0300

    Done

--- backend/Domain/MailSettings.cs
+++ backend/Domain/MailSettings.cs
@@ -3,9 +3,9 @@
     public class MailSettings
     {
         public string? Email { get; set; }
-        public string ToEmail { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
+        public string? ToEmail { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
         public string? DisplayName { get; set; }
         public string? Password { get; set; }
         public string? Host { get; set; }
