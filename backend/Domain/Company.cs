285cb45fae0189e4c427a2640eb55d706a3482bf
moryno <mauricenganga41@gmail.com>
2024-11-09 18:12:03 +0300

    Done

--- backend/Domain/Company.cs
+++ backend/Domain/Company.cs
@@ -4,9 +4,10 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public virtual ICollection<AppUser> Users { get; set; }
-        public virtual ICollection<AppRole> Roles { get; set; }
-        public virtual ICollection<Project> Projects { get; set; }
-        public virtual ICollection<Bug> Bugs { get; set; } 
+        public virtual ICollection<AppUser>? Users { get; set; }
+        public virtual ICollection<AppRole>? Roles { get; set; }
+        public virtual ICollection<Project>? Projects { get; set; }
+        public virtual ICollection<Bug>? Bugs { get; set; } 
+        public virtual ICollection<Event>? Events { get; set; } 
     }
 }
