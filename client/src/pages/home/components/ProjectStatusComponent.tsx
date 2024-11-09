2325808735d2fbc1ad8cae92d04b733fc76ed13a
moryno <mauricenganga41@gmail.com>
2024-11-09 17:28:10 +0300

    Done

--- client/src/pages/home/components/ProjectStatusComponent.tsx
+++ client/src/pages/home/components/ProjectStatusComponent.tsx
@@ -1,5 +1,9 @@
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Legend } from 'recharts';
 import { StyledChartCardHeaderImg, StyledChartCardHeaderTitle, StyledChartCardHeaderWrapper, StyledChartCardWrapper } from '../index.styled';
+import { useNavigate } from 'react-router-dom';
+import { setParams } from '_helpers';
+import { PROJECT_ROUTE } from '_constants';
+import { useCallback } from 'react';
 
 const ProjectStatusComponent = ({ projectStatus } : { projectStatus : any[]}) => {
 
@@ -7,12 +11,28 @@ const ProjectStatusComponent = ({ projectStatus } : { projectStatus : any[]}) =>
     const [name, value] = Object.entries(item)[0]; 
     return { name, value };
   });
+  const navigate = useNavigate();
+  
+  const handleClick = (e: any) => {
+      if(e){
+        if(e.name === "Active"){
+          let url = setParams("isActive", "True");
+          navigate(`${PROJECT_ROUTE}?${url}`)
+        }else{
+          let url = setParams("isActive", "False");
+          navigate(`${PROJECT_ROUTE}?${url}`)
+        }
+      }
+  }
+  const handleNavigate = useCallback(() => {
+    navigate(PROJECT_ROUTE)
+  }, [navigate])
 
   return (
     <StyledChartCardWrapper>
        <StyledChartCardHeaderWrapper>
         <StyledChartCardHeaderTitle>Project Status</StyledChartCardHeaderTitle>
-        <StyledChartCardHeaderImg src="/img/moreDark.png" alt="this is an elipses" />
+        <StyledChartCardHeaderImg onClick={handleNavigate} src="/img/moreDark.png" alt="this is an elipses" />
       </StyledChartCardHeaderWrapper> 
       <ResponsiveContainer height="90%">
         <PieChart>
@@ -24,6 +44,7 @@ const ProjectStatusComponent = ({ projectStatus } : { projectStatus : any[]}) =>
             outerRadius={"70%"}
             fill="#8884d8"
             dataKey="value"
+            onClick={handleClick}
           > 
           {formattedStatusCount.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
