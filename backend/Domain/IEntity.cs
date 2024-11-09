77f5ac296ec87f1e41a612799fe27afdce51f5f4
moryno <mauricenganga41@gmail.com>
2024-11-09 17:11:49 +0300

    Done

---
+++ backend/Domain/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+    public interface IEntity
+    {
+        Guid Id { get; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedUser { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string UpdatedUser { get; set; }
+        public Guid CompanyId { get; set; }
+    }
+}
