599d59ba9b14ca8ea328da0baecf7b215a8b48b8
moryno <mauricenganga41@gmail.com>
2024-11-09 18:12:27 +0300

    Done

--- backend/Domain/BugComment.cs
+++ backend/Domain/BugComment.cs
@@ -3,8 +3,8 @@
     public class BugComment
     {
         public Guid Id { get; set; }
-        public virtual AppUser Author { get; set; }
-        public virtual Bug Bug { get; set; }
+        public virtual AppUser? Author { get; set; }
+        public virtual Bug? Bug { get; set; }
         public string Description { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
     }
