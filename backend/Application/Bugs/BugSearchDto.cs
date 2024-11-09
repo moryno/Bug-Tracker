088b7d977a1f0c810851e3f35f7a6c8060b9fea2
moryno <mauricenganga41@gmail.com>
2024-11-09 17:03:56 +0300

    Done

---
+++ backend/Application/Bugs/BugSearchDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Bugs
+{
+    public class BugSearchDto
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string BugName { get; set; } = string.Empty;
+        public string Severity { get; set; } = string.Empty;
+        public string Classification { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+        public string BugStatus { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public int Count { get; set; } = 0;
+    }
+}
