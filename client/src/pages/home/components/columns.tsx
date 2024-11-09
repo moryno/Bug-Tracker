13a590b1ed740fcabddd119fd2be01e7970fada0
moryno <mauricenganga41@gmail.com>
2024-11-09 17:25:52 +0300

    Done

--- client/src/pages/home/components/columns.tsx
+++ client/src/pages/home/components/columns.tsx
@@ -48,7 +48,7 @@ export const topProjectColumns = [
       key: 'projectStatus',
       render: (projectStatus: number) => {
         let color = projectStatus <= 50 ? 'red' : projectStatus > 50 && projectStatus <= 70 ? "geekblue" : 'green';
-        return <Tag color={color}>{Math.round(projectStatus).toFixed(1)}%</Tag>;
+        return <Tag color={color}>{Math.round(projectStatus).toFixed(0)}%</Tag>;
       },
       width: 100,
     },
@@ -60,4 +60,51 @@ export const topProjectColumns = [
       width: 100
     }
   ];
-  
+  export const profileProjectColumns = [
+    {
+      title: 'Project Name',
+      key: 'projectName',
+      ellipsis: true,
+      render: (project : IProject) => (
+        <>
+            <StyledWorkItemInfoWrapper>
+              <StyledWorkItemInfoTitle>{ project.projectName }</StyledWorkItemInfoTitle>
+              <StyledWorkItemInfoDesc>{ project.description }</StyledWorkItemInfoDesc>
+            </StyledWorkItemInfoWrapper>
+        </>
+      ),
+    },
+    {
+      title: 'Priority',
+      dataIndex: 'priority',
+      key: 'priority',
+      render: (priority: string) => {
+        let color = priority === 'High' ? 'red' : priority === "Medium" ? "geekblue" : 'green';
+        return <Tag color={color}>{priority.toUpperCase()}</Tag>;
+      },
+      width: 100,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'projectStatus',
+      key: 'projectStatus',
+      render: (projectStatus: number) => {
+        let color = projectStatus <= 50 ? 'red' : projectStatus > 50 && projectStatus <= 70 ? "geekblue" : 'green';
+        return <Tag color={color}>{Math.round(projectStatus).toFixed(1)}%</Tag>;
+      },
+      width: 100,
+    },
+    {
+      title: 'End Date',
+      dataIndex: 'endDate',
+      key: 'endDate',
+      render: (date: string) => moment(date).format('YYYY-MM-DD'),
+      width: 100
+    },
+    {
+      title: 'Created By',
+      dataIndex: 'createdUser',
+      key: 'createdUser',
+      width: 150
+    },
+  ];
