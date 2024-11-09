f3b36dc871ca36d99669720d6bf6cec40d931a6e
moryno <mauricenganga41@gmail.com>
2024-11-09 17:32:50 +0300

    Done

--- client/src/pages/bugs/components/BugDetail.tsx
+++ client/src/pages/bugs/components/BugDetail.tsx
@@ -1,117 +1,97 @@
-import { Card, Divider, message, Tabs, TabsProps } from "antd";
-import { Comment, CommentForm, DetailPage, DescriptionComponent } from "_lib";
+import { message, Tabs, TabsProps } from "antd";
+import { DetailPage, DescriptionComponent, AppComment, AppLoader } from "_lib";
 import BugDetailHeader from "./BugDetailHeader";
 import BugInformation from "./BugInformation";
 import BugStatusComponent from "./BugStatusComponent";
-import { useCreateService, useGetById } from "_hooks";
+import { useDeleteRecord, useGetById } from "_hooks";
 import { BugService } from "_services";
 import { DomianEnum } from "_constants";
 import { useParams } from "react-router-dom";
 import { useCallback, useMemo, useState } from "react";
-import { commentType } from "interfaces";
+import { IBug } from "interfaces";
+import BugForm from "./BugForm";
+import BugAssignees from "./BugAssignees";
 
 const queryString = `${DomianEnum.BUGS}-comments`
 
 const BugDetail = () => {
   const { id } = useParams();
-  const { getBug, getBugComments, createBugComment, deleteBugComment, editBugComment} = BugService;
-  const [editedComment, setEditedComment] = useState<commentType | null>(null);
-  const [statusMode, setStatusMode] = useState("CreateMode");
+  const { getBug, getBugComments,deleteBug, createBugComment, deleteBugComment, editBugComment } = BugService;
+  const deleteRecord = useDeleteRecord(deleteBug, DomianEnum.BUGS);
   const { isLoading, data } = useGetById(getBug, DomianEnum.BUGS, id);
-  const { isLoading: loading, data: comments, refetch } = useGetById(getBugComments, queryString, id);
-  const [commentLoading, setCommentLoading] = useState(false)
-  const creatComment = useCreateService(createBugComment, queryString);
-  const editComment = useCreateService(editBugComment, queryString);
+  const [open, setOpen] = useState(false);
 
+  const showDrawer = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setOpen(false);
+    // setSelectedRecord(null);
+    // setStatusMode("CreateMode")
+  }, []);
+
+  const handleDelete = useCallback(async (bug : IBug) => {
+    if(!bug) return message.warning("Please select a bug to delete");
+    const recordId  = bug.id!
+    
+    try {
+      await deleteRecord.mutateAsync(recordId)
+    } catch (error) {
+      console.log(error);
+    }
+  }, [deleteRecord]);
 
   const onChange = (key: string) => {
     console.log(key);
   };
-  const onEditChange = (comment: commentType) => {
-    setEditedComment({
-      description: comment.description,
-      id: comment.id,
-    });
-    setStatusMode("EditMode")
-  };
-
- const onAddComment = useCallback(async (comment: commentType | null) => {
-  if(!comment) return message.warning("No comment to submit.");
-
-   setCommentLoading(true);
-
-  try {
-    if(statusMode === "CreateMode"){
-      const params = {
-        bugId: id,
-        description: comment.description
-        }
-      await creatComment.mutateAsync(params)
-    }else{
-      const params = {
-        bugId: id,
-        description: comment.description,
-        id: comment.id
-      }
-      await editComment.mutateAsync(params)
-    }
-    refetch()
-    setCommentLoading(false);
-    setStatusMode("CreateMode");
-    setEditedComment(null)
-  } catch (error) {
-    setCommentLoading(false);
-    console.log(error)
-  }
- }, [creatComment, editComment, id, refetch, statusMode]);
-
 
   const items: TabsProps["items"] = useMemo(() => ([
     {
       key: "1",
-      label: "Comments",
+      label: `Comments`,
       children: (
-        <Card>
-          <Comment 
-            comments={comments?.data}
-            loading={loading}
-            deleteService={deleteBugComment}
-            queryString={queryString}
-            onEditChange={onEditChange}
-           />
-           <Divider />
-          <CommentForm 
-            key={statusMode && editedComment?.id}
-            onAddComment={onAddComment}
-            loading={commentLoading}
-            editedComment={editedComment}
-            statusMode={statusMode}
-           />
-        </Card>
+        <AppComment 
+          parentId={id!}
+          parentIdIdentifier={"bugId"}
+          getService={getBugComments}
+          deleteService={deleteBugComment}
+          editService={editBugComment}
+          queryString={queryString}
+          createService={createBugComment}
+        />
       ),
     },
     {
       key: "2",
-      label: "Bugs",
-      children: "Content of Tab Pane 2",
+      label: "Bug Assignees",
+      children:     
+        <BugAssignees bugId={id!} />,
     },
-    {
-      key: "3",
-      label: "Tab 3",
-      children: "Content of Tab Pane 3",
-    },
-  ]), [commentLoading, comments?.data, deleteBugComment, editedComment, loading, onAddComment, statusMode]);
+  ]), [createBugComment, deleteBugComment, editBugComment, getBugComments, id]);
 
-  if(isLoading) return <p>Loading...</p>
+  if(isLoading) return <AppLoader />
   
+
   return (
+    <>
     <DetailPage>
-      <BugDetailHeader bug={data?.data} />
+      <BugDetailHeader bug={data?.data} openForm={showDrawer} deletBug={handleDelete} />
       <BugStatusComponent status={data?.data?.bugStatus} />
       <DescriptionComponent description={data?.data?.description} />
       <BugInformation bug={data?.data}  />
       <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
     </DetailPage>
+    {open &&
+      <BugForm 
+        key={data?.data?.id}
+        open={open} 
+        onClose={onClose} 
+        editedRecord={data?.data} 
+        statusMode={"EditMode"}
+      />
+    }
+    </>
   );
 };
 
