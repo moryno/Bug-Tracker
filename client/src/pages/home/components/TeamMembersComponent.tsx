9cd808d8958f55e79201909a458dff1a3bd8e0aa
moryno <mauricenganga41@gmail.com>
2024-11-09 17:27:31 +0300

    Done

--- client/src/pages/home/components/TeamMembersComponent.tsx
+++ client/src/pages/home/components/TeamMembersComponent.tsx
@@ -13,16 +13,65 @@ import {
     StyledWorkItemRightDiv, 
     StyledWorkItemWrapper 
 } from '../index.styled';
+import { IUser } from 'interfaces';
+import { useNavigate } from 'react-router-dom';
+import { useCallback, useState } from 'react';
+import { PROFILE_ROUTE, USER_ROUTE } from '_constants';
+import { EmailContainer } from '_lib';
+import { useAuthUser } from '_hooks';
 
-export const TeamMembersComponent = ({ team} : { team : any[]}) => {
+export const TeamMembersComponent = ({ team} : { team : IUser[]}) => {
+    const { user: currentUser } = useAuthUser();
+    const navigate = useNavigate();
+    const [lastClickTime, setLastClickTime] = useState(0);
+    const [open, setOpen] = useState(false);
+    const [selectedEmail, setSelectedEmail] = useState<string | null>(null);
+
+    const onRowClick = useCallback((record: IUser) => {
+        const currentTime = new Date().getTime();
+        const timeSinceLastClick = currentTime - lastClickTime;
+
+        if (record && timeSinceLastClick < 300) {
+            navigate(`${PROFILE_ROUTE}/${record.userName}`)
+          }
+      
+          setLastClickTime(currentTime);
+
+    }, [lastClickTime, navigate])
+
+    const viewProfile = useCallback((record: IUser) => {
+
+        if (record) {
+            navigate(`${PROFILE_ROUTE}/${record.userName}`)
+          }
+      
+    }, [navigate])
+
+
+    const sendMailToMember = useCallback((user: IUser) => {
+        if(user.email){
+            setOpen(true);
+            setSelectedEmail(user.email)
+        }
+
+    }, [])
+    const closeModal = useCallback(() => {
+        setOpen(false);
+        setSelectedEmail(null)
+    }, [])
+      
+    const handleNavigate = useCallback(() => {
+          navigate(USER_ROUTE)
+      }, [navigate])
+
   return (
     <StyledMiddleChartCardWrapper className='scrollbar-hide'>
     <StyledChartCardHeaderWrapper className='mb-2'>
      <StyledChartCardHeaderTitle>Team</StyledChartCardHeaderTitle>
-      <StyledChartCardHeaderImg src="/img/moreDark.png" alt="this is an elipses" />
+      <StyledChartCardHeaderImg onClick={handleNavigate} src="/img/moreDark.png" alt="this is an elipses" />
     </StyledChartCardHeaderWrapper> 
     { team && team?.map(( user : any) => (
-        <StyledWorkItemWrapper key={user?.id}>
+        <StyledWorkItemWrapper onClick={() => onRowClick(user)} key={user?.id}>
             <StyledWorkItemLeftDiv>
                 <StyledAssigneeImage src={ user?.image || "/img/noavatar.jpg" } />
                 <StyledWorkItemInfoWrapper>
@@ -31,13 +80,21 @@ export const TeamMembersComponent = ({ team} : { team : any[]}) => {
                 </StyledWorkItemInfoWrapper>
             </StyledWorkItemLeftDiv>
             <StyledWorkItemRightDiv>
-                <StyledWorkItemProfileIcon />
-                <StyledWorkItemMailIcon />
+                <StyledWorkItemProfileIcon onClick={() => viewProfile(user)} />
+               {user?.userName !== currentUser?.userName &&
+                <StyledWorkItemMailIcon onClick={() => sendMailToMember(user)} />
+               }
             </StyledWorkItemRightDiv>
         </StyledWorkItemWrapper>
     ))}
-
-    
+    {
+    open &&
+    <EmailContainer 
+        open={open} 
+        onClose={closeModal} 
+        recipientEmail={selectedEmail}
+     />
+    }
    </StyledMiddleChartCardWrapper>
   )
 }
