d5a0d66fce8eb7000e15e22aa17b458be5100229
moryno <mauricenganga41@gmail.com>
2024-11-09 17:35:14 +0300

    Done

--- client/src/_services/user.service.ts
+++ client/src/_services/user.service.ts
@@ -1,18 +1,24 @@
-import { USER_API } from "_constants";
+import { INVITE_API, USER_API } from "_constants";
+import { PaginationResult } from "_lib";
+import { InviteUserType, IUser } from "interfaces";
 import { request } from "utils";
 
 const getAllUsers = () => {
   return request.get(`${USER_API}/GetAll`);
 };
-const getTeamMembers = () => {
-  return request.get(USER_API);
+const getTeamMembers = (params?: object) => {
+  return request.get<PaginationResult<IUser[]>>(USER_API, { params });
 };
 const getProfile = (userName: string) => {
   return request.get(`profile/${userName}`);
+};
+const inviteUser = (invite: InviteUserType) => {
+  return request.post(INVITE_API, invite);
 };
 
 export const UserService = {
   getAllUsers,
   getTeamMembers,
   getProfile,
+  inviteUser,
 };
