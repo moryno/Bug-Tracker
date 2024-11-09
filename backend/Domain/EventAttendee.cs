e37d9a4463d3d2d1715ee907d068939e671cfa87
moryno <mauricenganga41@gmail.com>
2024-11-09 17:10:24 +0300

    Done

---
+++ backend/Domain/EventAttendee.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class EventAttendee
+    {
+        public Guid Id { get; set; }
+        public Guid? EventId { get; set; }
+        public virtual Event? Event { get; set; }
+        public Guid? AppUserId { get; set; }
+        public virtual AppUser? AppUser { get; set; }
+    }
+}
