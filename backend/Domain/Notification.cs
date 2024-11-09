c044768f558806ba3d16922725b968ff69904515
moryno <mauricenganga41@gmail.com>
2024-11-09 17:12:19 +0300

    Done

---
+++ backend/Domain/Notification.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool IsRead { get; set; } = false;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ReadAt { get; set; }
+        public Guid EventId { get; set; }
+        public virtual Event? Event { get; set; }
+        public Guid RecipientId { get; set; }
+        public virtual AppUser? Recipient { get; set; }
+        public Guid SenderId { get; set; }
+        public virtual AppUser? Sender { get; set; }
+    }
+}
