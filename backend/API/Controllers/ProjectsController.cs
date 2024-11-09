5bf46d45a1fa3c060d732c960c7b3555f92542b4
moryno <mauricenganga41@gmail.com>
2024-11-09 18:31:06 +0300

    Done

--- backend/API/Controllers/ProjectsController.cs
+++ backend/API/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
 using Application.Bugs;
+using Application.Core;
 using Application.Projects;
 using Domain;
 using MediatR;
@@ -13,9 +14,15 @@ namespace API.Controllers
 
         [HttpGet]
         [Authorize]
-        public async Task<ActionResult<List<ProjectDto>>> Get ()
+        public async Task<ActionResult<PagedList<ProjectDto>>> Get ([FromQuery]ProjectParams param)
         {
-            return await Mediator.Send(new List.Query());
+            return HandlePagedResult(await Mediator.Send(new List.Query { Params = param }));
+        }
+        [HttpGet("search")]
+        [Authorize]
+        public async Task<ActionResult<PagedList<ProjectSearchDto>>> SearchProject ([FromQuery]ProjectParams param)
+        {          
+            return HandlePagedResult(await Mediator.Send(new GetSearchProjects.Query { Params = param }));
         }
 
         [HttpGet("{id}")]
@@ -26,14 +33,14 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        [Authorize]
+        [Authorize(Roles = "Admin, Project Manager")]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await Mediator.Send(command);
         }
 
         [HttpPut("{id}")]
-        //[Authorize(Policy = "IsProjectOwner")]
+        [Authorize(Roles = "Admin, Project Manager")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
             command.Id = id;
@@ -41,8 +48,9 @@ namespace API.Controllers
         }
 
         [HttpDelete("{id}")]
-       // [Authorize(Policy = "IsProjectOwner")]
+        [Authorize(Roles = "Admin, Project Manager")]
         public async Task<ActionResult<Unit>> Delete(Guid id) => await Mediator.Send(new Delete.Command { Id = id });
+
         [HttpPost("comment")]
         [Authorize]
         public async Task<ActionResult<CommentDto>> CreateComment(CreateProjectComment.Command command) => Ok(await Mediator.Send(command));
@@ -62,5 +70,12 @@ namespace API.Controllers
         [HttpDelete("{id}/comment")]
         [Authorize]
         public async Task<ActionResult<Unit>> DeleteComment(Guid id) => Ok(await Mediator.Send(new DeleteProjectComment.Command { Id = id }));
+
+        [HttpGet("{id}/bugs")]
+        [Authorize]
+        public async Task<ActionResult<List<BugDto>>> GetProjectBugs(Guid id)
+        {
+            return await Mediator.Send(new GetProjectbugs.Query { ProjectId = id });
+        }
     }
 }
