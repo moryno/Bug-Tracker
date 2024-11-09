5b4d764d22143e7a63ee362cce2cfa2d885b443f
moryno <mauricenganga41@gmail.com>
2024-11-09 17:55:00 +0300

    Done

--- client/src/_lib/ContainerDrawer.tsx
+++ client/src/_lib/ContainerDrawer.tsx
@@ -1,20 +1,22 @@
 import React, { memo, ReactNode } from 'react';
-import { Drawer, Space } from 'antd';
+import { Space } from 'antd';
 import ContainerButton from './ContainerButton';
+import { StyledContainerDrawer } from './StyledComponents';
 interface IProps{
     onClose?: () => void;
     open?: boolean;
     title?: string;
     children?: ReactNode;
     width?: number;
-    onFinish: () => void;
-    loading: boolean
+    onFinish?: () => void;
+    loading?: boolean;
+    footer?: boolean
 }
 
-const ContainerDrawer: React.FC<IProps> = ({ onFinish, onClose, open, title, children, width, loading}) => {
+const ContainerDrawer: React.FC<IProps> = ({ onFinish, onClose, open, title, children, width, loading, footer = true }) => {
   return (
     <>
-      <Drawer
+      <StyledContainerDrawer
         title={title}
         width={width}
         onClose={onClose}
@@ -25,16 +27,16 @@ const ContainerDrawer: React.FC<IProps> = ({ onFinish, onClose, open, title, chi
             paddingBottom: 80,
           },
         }}
-        
-        footer={
+        footer={ footer ?
           <Space>
             <ContainerButton disabled={loading} onClick={onClose} title="Close" size="middle" />
             <ContainerButton loading={loading} onClick={onFinish} type="primary" title="Submit" size="middle" />
           </Space>
+          : false
         }
       >
         { children }
-      </Drawer>
+      </StyledContainerDrawer>
     </>
   );
 };
