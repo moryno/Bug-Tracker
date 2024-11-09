7f1b0286de9887a988a82834add9205b52f27eb5
moryno <mauricenganga41@gmail.com>
2024-11-09 18:15:24 +0300

    Done

--- backend/Application/Users/GetTeamMembers.cs
+++ backend/Application/Users/GetTeamMembers.cs
@@ -1,18 +1,23 @@
-﻿using Application.Errors;
+﻿using Application.Core;
+using Application.Errors;
 using Application.Interfaces;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using System.Linq;
 using System.Net;
 
 namespace Application.Users
 {
     public class GetTeamMembers
     {
-        public class Query : IRequest<List<Domain.UserDto>> { }
-        public class Handler : IRequestHandler<Query, List<Domain.UserDto>>
+        public class Query : IRequest<PagedList<Domain.UserDto>> 
+        {
+            public UserParams? Params { get; set; }
+        }
+        public class Handler : IRequestHandler<Query, PagedList<Domain.UserDto>>
         {
             private readonly DataContext _context;
             private readonly IUserAccessor _userAccessor;
@@ -25,17 +30,14 @@ namespace Application.Users
                 _userManager = userManager;
             }
 
-            public async Task<List<Domain.UserDto>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<PagedList<Domain.UserDto>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUserName());
                 if (user == null)
                     throw new RestException(HttpStatusCode.NotFound, new { error = "User not found." });
-                var company = await _context.Companies.FindAsync(user.CompanyId);
-                if (company == null)
-                    throw new RestException(HttpStatusCode.NotFound, new { error = "Company not found." });
-
-                var users = await _context.Users
-                  .Where(u => u.CompanyId == company.Id)
+          
+                var query = _context.Users
+                  .Where(u => u.CompanyId == user.CompanyId)
                   .Select(u => new Domain.UserDto
                   {
                       Id = u.Id,
@@ -51,9 +53,23 @@ namespace Application.Users
                             .Join(_context.Roles, ur => ur.RoleId, r => r.Id, (ur, r) => r.Name)
                             .ToList()
                   })
-                  .ToListAsync();
+                .AsQueryable();
 
-                return users;
+                if (!String.IsNullOrEmpty(request.Params.FullName))
+                {
+                    query = query.Where(x => x.FullName.ToLower().Contains(request.Params.FullName.ToLower()));
+                }
+                if (!String.IsNullOrEmpty(request.Params.Role))
+                {
+                    query = query.Where(x => x.Roles.Any(r => r.Contains(request.Params.Role)));
+                }
+                if (!String.IsNullOrEmpty(request.Params.Status))
+                {
+                    var status = request.Params.Status == "Active" ? true : false;
+                    query = query.Where(x => x.Status == status);
+                }
+
+                return await PagedList<Domain.UserDto>.CreateAsync(query, request.Params.PageNumber, request.Params.PageSize);
 
             }
         }
